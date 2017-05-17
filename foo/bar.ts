91880b9f127286a34f40d098dbdbbce18719266a
david <david@spektrakel.de>
2017-05-17 12:02:05 +0200

    feat: Initial proof-of-concept compilation
    Demonstrate how to use the tools and in which order they need to run.

---
+++ foo/bar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bar-component',
+    templateUrl: './bar.html',
+    styleUrls: ['./bar.scss']
+})
+export class BarComponent {
+}
