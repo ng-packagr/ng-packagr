6d4a90efd265cb92bc7b3e691c63fdafffb7b800
David <david@spektrakel.de>
2017-05-19 21:11:00 +0200

    feat: cli command `ng-packagr` for npm script users

---
+++ cli/ng-packagr.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+"use strict";
+
+const path = require('path');
+
+// Read CLI arguments
+const ARGS = require('minimist')(process.argv.slice(2));
+
+const project = ARGS.p || ARGS.project || path.resolve(process.cwd(), '.ng-packagr.json');
+
+const ngPackagr = require('../dist/ng-packagr');
+
+ngPackagr.packageAngular({
+  project
+});
