1651cbd71344adf3cd633c26a8a31b175985942d
David <david@spektrakel.de>
2017-05-19 21:11:16 +0200

    build: add a simple release and publish workflow

--- prepublish.js
+++ prepublish.js
@@ -1,3 +1,13 @@
 const cpx = require('cpx');
+const fs = require('fs');
 
+cpx.copy('LICENSE', 'dist');
+cpx.copy('README.md', 'dist');
+cpx.copy('CHANGELOG.md', 'dist');
+cpx.copy('sample/**/*', 'dist/sample');
 
+const packageJson = JSON.parse(fs.readFileSync('package.json'));
+delete packageJson['devDependencies'];
+delete packageJson['scripts'];
+delete packageJson['private'];
+fs.writeFileSync('dist/package.json', JSON.stringify(packageJson, undefined, 2));
