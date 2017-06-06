f1af7ce46daa6d117ee5c335b3e25151b27b8f92
David Herges <dherges@users.noreply.github.com>
2017-06-06 08:22:34 +0200

    feat: see commit before

--- integration/sample_core/src/angular.service.ts
+++ integration/sample_core/src/angular.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AngularService {
+  
+  private _subject: Subject<any> = new ReplaySubject<any>(1);
 
   constructor(
     private http: Http
@@ -14,6 +18,10 @@ export class AngularService {
 
     return this.http.get('/foo/bar')
       .map((res: Response) => `${res.ok}`);
+  }
+
+  public get bar(): Observable<any> {
+    return this._subject.asObservable();
   }
 
 }
