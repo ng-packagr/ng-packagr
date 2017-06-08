c389160c1e9772ba56bbb811a1d1a573af22d3fc
David Herges <dherges@users.noreply.github.com>
2017-06-08 09:32:55 +0200

    fix: correct paths in generated sourcemaps (#50)
    When compiling with `tsc` (the FESM5 bundle file), the source map files goes corrupt. Manually fix paths there and enable the source map chain again. Source maps now reflect back to closure-annotated source files. See #46 
    
    Also, relocate source paths in final `*.js.map` files. Motivation: source files now show up with a common prefix `@<scope>/<name>`, making it more intuitive to find sources of the library in dev tools. For example, in `ng` CLI sources would show up in the `webpack://~/@sample/material`  or  `webpack://~/sample-custom` path.

--- lib/steps/json.ts
+++ lib/steps/json.ts
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const glob = require('glob')
+import * as mz from 'mz/fs';
 
 /**
  * Reads a JSON file.
@@ -44,3 +46,31 @@ export const writeJson = (object: any, file: string): Promise<any> => {
     });
   });
 }
+
+
+/**
+ * Modifies a set of JSON files by invoking `modifyFn`
+ *
+ * @param globPattern A glob pattern matching several files. Example: '**\/*.js.map'
+ * @param modifyFn A callback function that tajes a JSON-parsed input and should return an output
+ *                  that will be JSON-stringified
+ */
+export const modifyJsonFiles = (globPattern: string, modifyFn: (jsonObj: any) => any): Promise<void> => {
+
+  return new Promise<string[]>((resolve, reject) => {
+      glob(globPattern, (err, files: string[]) => {
+        if (err) {
+          reject(err);
+        }
+
+        resolve(files);
+      });
+    })
+    .then((fileNames: string[]): Promise<string[]> => Promise.all(
+      fileNames.map((fileName: string): Promise<string> => mz.readFile(fileName)
+        .then((fileContent: string) => mz.writeFile(fileName, JSON.stringify(modifyFn(JSON.parse(fileContent)))))
+      ))
+    )
+    .then(() => Promise.resolve());
+
+};
