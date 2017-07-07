a90079fe35e231b7a96a3d31946dbcf1d7faa752
David Enke <david.enke@zalari.de>
2017-07-07 15:36:23 +0200

    updated material integration sample to import scss var from node_modules

--- integration/sample_material/public_api.ts
+++ integration/sample_material/public_api.ts
@@ -1,3 +1,4 @@
 export * from './bar/bar.component';
 export * from './foo/foo.component';
+export * from './baz/baz.component';
 export * from './ui-lib.module';
--- integration/sample_material/ui-lib.module.ts
+++ integration/sample_material/ui-lib.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { FooComponent } from './foo/foo.component';
 import { BarComponent } from './bar/bar.component';
 import { FooBarComponent } from './foo-bar/foo-bar.component';
+import { BazComponent } from './baz/baz.component';
 
 @NgModule({
   declarations: [
     FooComponent,
     BarComponent,
     FooBarComponent,
+    BazComponent,
   ],
   exports: [
     FooComponent,
     BarComponent,
     FooBarComponent,
+    BazComponent,
   ]
 })
 export class UiLibModule {}
