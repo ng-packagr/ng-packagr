2e86bbbe0b65066504ef95413773e2b465467bc8
David <david@spektrakel.de>
2017-08-05 23:07:30 +0200

    refactor: move consumers and samples to a dedicated folder
    a little refactoring never killed nobody, errr... yes :-)

--- integration/samples.js
+++ integration/samples.js
@@ -2,20 +2,12 @@ const path = require('path');
 process.env.DEBUG = true;
 
 const SAMPLES = [
-  path.resolve(__dirname, 'sample_core', 'ng-package.json'),
-  path.resolve(__dirname, 'sample_custom', 'ng-package.json'),
-  path.resolve(__dirname, 'sample_material', 'ng-package.json'),
-  path.resolve(__dirname, 'sample_rxjs', 'ng-package.json'),
-  path.resolve(__dirname, 'sample_typings', 'ng-package.json'),
+  path.resolve(__dirname, 'samples', 'core', 'ng-package.json'),
+  path.resolve(__dirname, 'samples', 'custom', 'ng-package.json'),
+  path.resolve(__dirname, 'samples', 'material', 'ng-package.json'),
+  path.resolve(__dirname, 'samples', 'rxjs', 'ng-package.json'),
+  path.resolve(__dirname, 'samples', 'typings', 'ng-package.json'),
 ];
-
-/*
-    "samples": "node dist/cli/ng-packagr.js -p integration/sample/ng-package.json",
-    "sample:dev": "node integration/build.js  -p integration/sample/ng-package.json",
-    "sample:test": "mocha --compilers ts:ts-node/register integration/sample/test/package.ts",
-    "sampleng": "node dist/cli/ng-packagr.js -p integration/sample_angular/ng-package.json",
-    "sampleng:dev": "node integration/build.js -p integration/sample_angular/ng-package.json",
-*/
 
 // @see https://github.com/TypeStrong/ts-node#programmatic-usage
 require('ts-node').register({
--- integration/samples.sh
+++ integration/samples.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-node dist/cli/ng-packagr.js -p integration/sample_core/ng-package.json
-node dist/cli/ng-packagr.js -p integration/sample_custom/ng-package.json
-node dist/cli/ng-packagr.js -p integration/sample_material/ng-package.json
+node dist/cli/ng-packagr.js -p integration/samples/core/ng-package.json
+node dist/cli/ng-packagr.js -p integration/samples/custom/ng-package.json
+node dist/cli/ng-packagr.js -p integration/samples/material/ng-package.json
+node dist/cli/ng-packagr.js -p integration/samples/rxjs/ng-package.json
+node dist/cli/ng-packagr.js -p integration/samples/typings/ng-package.json
