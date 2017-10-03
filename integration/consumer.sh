25ec251b2905dad74fcb80de6b59ac64f62746fe
David Herges <dherges@users.noreply.github.com>
2017-10-03 10:31:59 +0200

    ci: include ng cli consumer build (#146)

--- integration/consumer.sh
+++ integration/consumer.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
-cd integration/sample_custom
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+echo "Running consumer builds in $parent_path"
+
+# Prepare 'sample-custom'
+pushd samples/custom/dist
 yarn unlink
 yarn link
-cd ../..
+popd
 
-cd integration/consumer-ng-cli
-yarn link sample-custom
+# Prepare '@sample/material'
+pushd samples/material/dist
+yarn unlink
+yarn link
+popd
+
+# Build ng cli app
+pushd consumers/ng-cli
 yarn install
+yarn link sample-custom
+yarn link @sample/material
 yarn build:dev
 yarn build:prod:jit
 yarn build:prod:aot
-cd ../..
+popd
 
-node_modules/.bin/tsc -p integration/consumer-tsc/tsconfig.json --target es2015 --module es2015
-node_modules/.bin/tsc -p integration/consumer-tsc/tsconfig.json --target es5 --module es2015
-node_modules/.bin/tsc -p integration/consumer-tsc/tsconfig.json --target es5 --module umd
-node_modules/.bin/tsc -p integration/consumer-tsc/tsconfig.json --target es5 --module commonjs
+
+
+# node_modules/.bin/tsc -p integration/consumer-tsc/tsconfig.json --target es2015 --module es2015
+# node_modules/.bin/tsc -p integration/consumer-tsc/tsconfig.json --target es5 --module es2015
+# node_modules/.bin/tsc -p integration/consumer-tsc/tsconfig.json --target es5 --module umd
+# node_modules/.bin/tsc -p integration/consumer-tsc/tsconfig.json --target es5 --module commonjs
