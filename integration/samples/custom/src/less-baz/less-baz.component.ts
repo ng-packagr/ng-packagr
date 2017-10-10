ddbf2b35da13c10573186f963216551baa45228f
david <david@spektrakel.de>
2017-10-10 13:44:29 +0200

    test: empty `.less` style file causes build error
    See #165

---
+++ integration/samples/custom/src/less-baz/less-baz.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'baz',
+  template: '<h2>LessBaz works!</h2>',
+  styleUrls: ['./less-baz.component.less']
+})
+export class LessBazComponent {}
