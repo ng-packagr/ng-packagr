26748b0cc17d91c4d2e6011cd1b73a57334ddbe2
David Parks <davidparks8@outlook.com>
2017-10-21 15:18:20 -0500

    fix: replace win path separators on broken node versions

--- src/lib/util/path.ts
+++ src/lib/util/path.ts
@@ -8,6 +8,9 @@ export const path = {
       return null;
     }
 
-    return path.replace(nodePath.win32.sep, '/');
+    // we use a regex instead of the character literal due to a bug in some versions of node.js
+    // the path separator needs to be preceded by an escape character
+    const regex = new RegExp('\\' + nodePath.win32.sep, 'g');
+    return path.replace(regex, nodePath.posix.sep);
   }
 };
