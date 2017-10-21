9a754be20f0b22f0417361f47038d4b837bdef27
David Parks <davidparks8@outlook.com>
2017-10-21 14:30:17 -0500

    test: ensure minified bundles are produced

--- integration/samples/secondary/specs/assets.ts
+++ integration/samples/secondary/specs/assets.ts
@@ -19,10 +19,42 @@ describe(`@sample/secondary`, () => {
     });
   });
 
+  describe(`secondary-lib.umd.min.js`, () => {
+    let GENERATED;
+    before(done => {
+      GENERATED = require(path.resolve(__dirname, '..', 'dist', 'bundles', 'secondary-lib.umd.min.js'));
+      done();
+    });
+
+    it(`should exist`, () => {
+      expect(GENERATED).to.be.ok;
+    });
+
+    it(`should have "BazComponent"`, () => {
+      expect(GENERATED.BazComponent).to.be.ok;
+    });
+  });
+
   describe(`secondary-lib-sub-module.umd.js`, () => {
     let GENERATED;
     before(done => {
       GENERATED = require(path.resolve(__dirname, '..', 'dist', 'bundles', 'secondary-lib-sub-module.umd.js'));
+      done();
+    });
+
+    it(`should exist`, () => {
+      expect(GENERATED).to.be.ok;
+    });
+
+    it(`should have "BarComponent"`, () => {
+      expect(GENERATED.BarComponent).to.be.ok;
+    });
+  });
+
+  describe(`secondary-lib-sub-module.umd.min.js`, () => {
+    let GENERATED;
+    before(done => {
+      GENERATED = require(path.resolve(__dirname, '..', 'dist', 'bundles', 'secondary-lib-sub-module.umd.min.js'));
       done();
     });
 
