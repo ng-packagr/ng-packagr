3fd6d78f2a31f1ed062249efc036f21fc305c515
david <david@spektrakel.de>
2017-11-10 13:38:10 +0100

    test: fix compile error in consumer app caused by unregistered comp
    LessBazComponent was not added to any NgModule, thus causing a compilation error in the Angular CLI consumer app. Add the component to a NgModule

--- integration/samples/custom/src/custom.module.ts
+++ integration/samples/custom/src/custom.module.ts
@@ -5,11 +5,12 @@ import { FooComponent } from './foo/foo.component';
 import { BarComponent } from './bar/bar.component';
 import { BazComponent } from './baz/baz.component';
 import { FooBarComponent } from './foo-bar/foo-bar.component';
+import { LessBazComponent } from './less-baz/less-baz.component';
 import { InternalService } from './internal.service';
 
 @NgModule({
   imports: [ CommonModule ],
-  declarations: [ FooComponent, BarComponent, BazComponent, FooBarComponent ],
+  declarations: [ FooComponent, BarComponent, BazComponent, FooBarComponent, LessBazComponent ],
   exports: [ FooComponent, BarComponent, BazComponent, FooBarComponent ]
 })
 export class CustomModule {
