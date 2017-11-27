d15a361efaf30839d69a7ce833d0edbe500614eb
Alan Agius <alan.agius4@gmail.com>
2017-11-27 17:02:08 +0100

    refactor: avoid name collisions by creating sub-folders

--- src/lib/model/ng-artifacts-factory.ts
+++ src/lib/model/ng-artifacts-factory.ts
@@ -1,8 +1,30 @@
+import { pathExistsSync } from 'fs-extra';
 import { path } from './../util/path';
 import { NgArtifacts } from './ng-artifacts';
 import { NgPackageData, SCOPE_NAME_SEPARATOR } from './ng-package-data';
 
 export class NgArtifactsFactory {
+
+  private _makeEsmPackageNameVal: string | undefined;
+  private _makeEsmPackageName(ngPkg: NgPackageData): string {
+    // The below is done in order to avoid same collisions in package name.
+    if (this._makeEsmPackageNameVal) {
+      return this._makeEsmPackageNameVal;
+    }
+
+    const { packageNameWithoutScope, flatModuleFileName, buildDirectory } = ngPkg;
+
+    if (packageNameWithoutScope === flatModuleFileName) {
+      this._makeEsmPackageNameVal = `${flatModuleFileName}.js`;
+    } else {
+      const modulePath = path.join(buildDirectory, 'esm2015', `${flatModuleFileName}.js`);
+      const pkgName = pathExistsSync(modulePath) ? packageNameWithoutScope : flatModuleFileName;
+      this._makeEsmPackageNameVal = `${pkgName}.js`;
+    }
+
+    return this._makeEsmPackageNameVal;
+  }
+
   private _makeUmdPackageName(ngPkg: NgPackageData): string {
     return ngPkg.packageNameWithoutScope.replace(SCOPE_NAME_SEPARATOR, '-') + '.umd.js';
   }
@@ -18,8 +40,8 @@ export class NgArtifactsFactory {
 
     return {
       main: path.join(buildDirectory, 'bundles', this._makeUmdPackageName(ngPkg)),
-      module: path.join(buildDirectory, 'esm5', `${flatModuleFileName}.js`),
-      es2015: path.join(buildDirectory, 'esm2015', `${flatModuleFileName}.js`),
+      module: path.join(buildDirectory, 'esm5', this._makeEsmPackageName(ngPkg)),
+      es2015: path.join(buildDirectory, 'esm2015', this._makeEsmPackageName(ngPkg)),
       typings: path.join(pathFromRoot, `${flatModuleFileName}.d.ts`),
       metadata: path.join(pathFromRoot, `${flatModuleFileName}.metadata.json`)
     }
@@ -31,8 +53,8 @@ export class NgArtifactsFactory {
 
     return {
       main: this._unixPathJoin(rootPathFromSelf, 'bundles', this._makeUmdPackageName(ngPkg)),
-      module: this._unixPathJoin(rootPathFromSelf, 'esm5', `${flatModuleFileName}.js`),
-      es2015: this._unixPathJoin(rootPathFromSelf, 'esm2015', `${flatModuleFileName}.js`),
+      module: this._unixPathJoin(rootPathFromSelf, 'esm5', this._makeEsmPackageName(ngPkg)),
+      es2015: this._unixPathJoin(rootPathFromSelf, 'esm2015', this._makeEsmPackageName(ngPkg)),
       typings: path.ensureUnixPath(`${flatModuleFileName}.d.ts`),
       metadata: path.ensureUnixPath(`${flatModuleFileName}.metadata.json`)
     }
