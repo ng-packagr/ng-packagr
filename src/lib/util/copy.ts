68d0c34710884428075c24570edbc37944c087ac
Alan Agius <alan.agius4@gmail.com>
2017-11-27 13:52:33 +0100

    fix: bump `commander` to `^2.12.0`, optimize typings for `cpx` and `commander` (#323)

--- src/lib/util/copy.ts
+++ src/lib/util/copy.ts
@@ -1,14 +1,14 @@
+import { copy } from 'cpx';
 import { promisify } from './promisify';
 import { debug } from './log';
-const cpx = require('cpx');
 
 export const copyFiles = (src: string, dest: string, options?: any): Promise<void> => {
   debug('copyFiles from ' + src + ' to ' + dest);
   return promisify<void>((resolveOrReject) => {
     if (options) {
-      cpx.copy(src, dest, options, resolveOrReject);
+      copy(src, dest, options, resolveOrReject);
     } else {
-      cpx.copy(src, dest, resolveOrReject);
+      copy(src, dest, resolveOrReject);
     }
   });
 
