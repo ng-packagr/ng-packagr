7de7e67bbcb63bbcc076d22fde4be78793dc5068
Alan Agius <alan.agius4@gmail.com>
2017-11-27 20:49:12 +0100

    test: and some more specs for same-name packages

--- integration/samples/same-name/specs/metadata.ts
+++ integration/samples/same-name/specs/metadata.ts
@@ -31,4 +31,32 @@ describe(`@sample/same-name`, () => {
     });
 
   });
+
+  describe(`testing/testing.metadata.json`, () => {
+    let METADATA;
+    before(() => {
+      METADATA = require('../dist/testing/testing.metadata.json');
+    });
+
+    it(`should exist`, () => {
+      expect(METADATA).to.be.ok;
+    });
+
+    it(`should be version 4`, () => {
+      expect(METADATA.version).to.equal(4);
+    });
+
+    it(`should be version 4`, () => {
+      expect(METADATA.version).to.equal(4);
+    });
+
+    it(`should "importAs": "@sample/testing"`, () => {
+      expect(METADATA['importAs']).to.equal('@sample/testing/testing');
+    });
+
+    it(`should be "__symbolic": "module"`, () => {
+      expect(METADATA['__symbolic']).to.equal('module');
+    });
+
+  });
 });
