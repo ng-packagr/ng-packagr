b9a2d53d007c79e7ebfe8168110bd453a86ef62a
David <david@spektrakel.de>
2017-12-02 13:13:19 +0100

    refactor: clarify on the terms _Package_ and _Entry Point_

--- src/lib/domain/ng-package.ts
+++ src/lib/domain/ng-package.ts
@@ -1,21 +1,50 @@
+import * as path from 'path';
 import { SchemaClass } from '@ngtools/json-schema';
 import { NgPackageConfig } from '../../ng-package.schema';
 
 /**
  * An Angular package being built.
  *
- * #### Relationship in the domain
+ * #### Representation in the domain
  *
- * An `NgPackage` is reflected by exactly one distribution-ready npm package.
- * An `NgPackage` is composed of at least one entrypoint, one primary entrypoint and
- * zero or more secondary entrypoints. Each of those entrypoints is considered a library and
- * reflected in `NgLibrary`.
+ * Angular Package Format defines the terms _Package_ and _Entry Point_.
+ * A _Package_ is a "set of files that are published to NPM and installed together".
+ * An _Entry Point_ is "referenced by a unique module ID and exports the public API referenced
+ * by that module ID. An example is @angular/core or @angular/core/testing. Both entry points
+ * exist in the @angular/core package, but they export different symbols. A package can have
+ * many entry points."
+ *
+ * The term 'Package' is reflected by the domain class `NgPackage`.
+ * An `NgPackage` is transformed to exactly one distribution-ready npm package.
+ * Further, an `NgPackage` is composed of at least one entrypoint, one primary entry point and
+ * zero or more secondary entry points.
+ * Each of those entrypoints is reflected by `NgLibrary` and is considered the root of the source
+ * code compilation / transformation process.
+ *
+ * #### Watch Out
+ *
+ * The user's configuration `ngPackage` suggests that the configuration object is reflected by
+ * `NgPackage`. Is is not.
+ *
+ * The user's `ngPackage` configueation is represented in `NgEntryPoint`. In case of the
+ * _Package_ (`NgPackage`), the configuration is reflected in the primary `NgEntryPoint`.
+ *
+ * @link https://docs.google.com/document/d/1CZC2rcpxffTDfRDs6p1cfbmKNLA6x5O-NtkJglDaBVs/edit#
  */
 export class NgPackage {
 
   constructor(
-    public readonly primary: NgLibrary,
-    public readonly secondaries: NgLibrary[] = []
+    private readonly basePath: string,
+
+    /**
+     * A reference to the primary entry point.
+     */
+    public readonly primary: NgEntryPoint,
+
+    /**
+     * An array of seconary entry points.
+     */
+    public readonly secondaries: NgEntryPoint[] = []
   ) {}
 
 }
@@ -27,8 +56,14 @@ export class NgPackage {
  *
  * _TBD_ the thing that - in effect - gets compiled from `*.ts`, `*.html`, `*.css` (and so on)
  * to FESM'5, FESM2015, UMD, AoT metadata, typings.
+ *
+ * An `NgEntryPoint` serves as the root of a library's source tree.
+ * During the compilation process (a tree transformation / transformation pipeline) it will be
+ * transpiled to a set of artefacts such as a FESM'5 bundle, a FESM2015 bundle, AoT metadata,
+ * and so on.
+ * The set of artefacts is reflected in `NgArtefacts`.
  */
-export class NgLibrary {
+export class NgEntryPoint {
 
   constructor(
     public readonly packageJson: any,
@@ -38,7 +73,7 @@ export class NgLibrary {
 
   // this.$schema.$$get('lib.flatModuleFile')
 
-  entryFile: Entrypoint;
+  entryFile: SourceFilePath;
 }
 
 
@@ -48,12 +83,5 @@ export class NgLibrary {
  * Typically, an entrypoint refers to the `public_api.ts` source file, referencing all other
  * source files that are considered in the compilation (transformation) process, as well as
  * describing the API surface of a library.
- *
- * #### Relationship in the domain
- *
- * An `Entrypoint` serves as the root of a library's source tree.
- * During the compilation process (a tree transformation / transformation pipeline) it will be
- * transpiled to a set of artefacts such as a FESM'5 bundle, a FESM2015 bundle, AoT metadata,
- * and so on. The set of artefacts is reflected in `NgArtefacts`.
  */
-export type Entrypoint = string;
+export type SourceFilePath = string;
