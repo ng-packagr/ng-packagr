f266fa94d525a57f9773411d1de929fddf1f4434
David <david@spektrakel.de>
2017-12-08 12:03:15 +0100

    build: convenient vscode launch scripts

--- integration/samples.dev.js
+++ integration/samples.dev.js
@@ -1,13 +1,18 @@
+#!/usr/bin/env node
 const fs = require('fs-extra');
 const path = require('path');
 process.env.DEBUG = true;
 
 const PATH = path.resolve(__dirname, 'samples');
-const SAMPLES = fs.readdirSync(PATH)
-  .map(dir => path.resolve(PATH, dir))
-  .filter(file => fs.lstatSync(file).isDirectory())
-  .map(dir => path.resolve(dir, 'ng-package.json'));
-
+let SAMPLES = [];
+if (process.argv[2]) {
+  SAMPLES = [ path.resolve(PATH, process.argv[2]) ];
+} else {
+  SAMPLES = fs.readdirSync(PATH)
+    .map(dir => path.resolve(PATH, dir))
+    .filter(file => fs.lstatSync(file).isDirectory())
+    .reverse();
+}
 
 // @see https://github.com/TypeStrong/ts-node#programmatic-usage
 require('ts-node').register({
@@ -21,7 +26,11 @@ while (SAMPLES.length > 0) {
   const project = SAMPLES.pop();
 
   promise = promise
-    .then(() => ngPackagr.createNgPackage({ project }))
+    .then(() => {
+      console.info(`$ ng-packagr -p ${project}`);
+
+      return ngPackagr.createNgPackage({ project });
+    })
     .catch((err) => {
       console.error('Samples failed.', err);
       process.exit(1);
