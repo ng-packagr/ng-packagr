b0e362e61b08b4bfd971648c2cc4e8e74e6daacc
David <david@spektrakel.de>
2017-12-10 20:57:49 +0100

    fix: print `@angular/compiler-cli` version

--- src/lib/version-info.ts
+++ src/lib/version-info.ts
@@ -1,15 +1,10 @@
 import * as rollup from 'rollup';
 import { VERSION as COMPILER_VERSION } from '@angular/compiler';
 import { VERSION as COMPILER_CLI_VERSION } from '@angular/compiler-cli';
-import { version as tsVersion } from 'typescript';
-
-const NGC_VERSION = COMPILER_VERSION.full;
-const CLI_VERSION = COMPILER_CLI_VERSION.full;
-const ROLLUP_VERSION = (rollup as any).VERSION;
-const TS_VERSION = tsVersion;
+import { version as TS_VERSION } from 'typescript';
 const TSICKLE_VERSION = require('tsickle/package.json').version;
-let NG_PACKAGR_VERSION: string = 'unknown';
 
+let NG_PACKAGR_VERSION: string = 'unknown';
 function tryReadVersion(paths: string[] = []) {
   if (paths.length === 0) {
     return;
@@ -30,9 +25,9 @@ tryReadVersion(['../../package.json', '../../../package.json']);
 
 export function printVersionInfo() {
   console.log(`ng-packagr:            ` + NG_PACKAGR_VERSION);
-  console.log(`@angular/compiler:     ` + NGC_VERSION);
-  console.log(`@angular/compiler-cli: ` + NGC_VERSION);
-  console.log(`rollup:                ` + ROLLUP_VERSION);
+  console.log(`@angular/compiler:     ` + COMPILER_VERSION.full);
+  console.log(`@angular/compiler-cli: ` + COMPILER_CLI_VERSION.full);
+  console.log(`rollup:                ` + (rollup as any).VERSION);
   console.log(`tsickle:               ` + TSICKLE_VERSION);
   console.log(`typescript:            ` + TS_VERSION);
 }
