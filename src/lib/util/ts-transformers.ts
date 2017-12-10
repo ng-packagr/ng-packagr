3e9cefb2174c6845d30eac2a2ed622100d0923cc
David <david@spektrakel.de>
2017-12-10 09:49:22 +0100

    fix: relax on non-call-expression decorators in typescript parsing

--- src/lib/util/ts-transformers.ts
+++ src/lib/util/ts-transformers.ts
@@ -2,16 +2,23 @@ import * as ts from 'typescript';
 import * as path from 'path';
 
 export const isComponentDecorator =
-  (node: ts.Node): node is ts.Decorator =>
-    ts.isDecorator(node) && node
-      .getChildren().find(ts.isCallExpression)
-      .getChildren().find(ts.isIdentifier)
-      .getText() === 'Component';
-      /* TODO: text comparison can break when
-       * `import { Component as foo } from '@angular/core'` or
-       * `import * as ng from '@angular/core'`
-       * @link https://github.com/angular/devkit/blob/master/packages/schematics/angular/utility/ast-utils.ts#L127-L128
-       */
+  (node: ts.Node): node is ts.Decorator => {
+    if (ts.isDecorator(node)) {
+      const callExpression = node.getChildren().find(ts.isCallExpression);
+      if (callExpression) {
+        const identifier = callExpression.getChildren().find(ts.isIdentifier);
+
+        return identifier && identifier.getText() === 'Component';
+        /* TODO: text comparison can break when
+        * `import { Component as foo } from '@angular/core'` or
+        * `import * as ng from '@angular/core'`
+        * @link https://github.com/angular/devkit/blob/master/packages/schematics/angular/utility/ast-utils.ts#L127-L128
+        */
+      }
+    }
+
+    return false;
+  }
 
 export const isPropertyAssignmentFor =
   (node: ts.Node, name: string): node is ts.PropertyAssignment =>
