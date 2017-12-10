7135c42d2a2b710d1de86a96a0ecca2c47376999
David <david@spektrakel.de>
2017-12-10 09:49:22 +0100

    fix: relax on non-call-expression decorators in typescript parsing

--- src/lib/util/ts-transformers.spec.ts
+++ src/lib/util/ts-transformers.spec.ts
@@ -1,23 +1,29 @@
+import { tags } from '@angular-devkit/core';
 import { expect } from 'chai';
 import * as ts from 'typescript';
 import { isComponentDecorator } from './ts-transformers';
 
+const createSourceFile = (sourceText: string, sourceName: string = 'foo.ts') =>
+  ts.createSourceFile(sourceName, sourceText, ts.ScriptTarget.ES5, true, ts.ScriptKind.TS);
+
 describe(`ts-transformers`, () => {
 
   describe(`isComponentDecorator`, () => {
-    const sourceFile = ts.createSourceFile('foo.ts',
-      `import {Component} from '@angular/core'; @Component({})class MyComponent{} @Directive()class Foo{}`,
-      ts.ScriptTarget.Latest, true, ts.ScriptKind.TS);
-    const decoratorNode = sourceFile.getChildAt(0).getChildAt(1)
-      .getChildAt(0).getChildAt(0);
+    const sourceFile = createSourceFile(tags.stripIndent`
+      import {Component} from '@angular/core';
+
+      @Component({})
+      @Other
+      class MyComponent {}
+    `);
 
     it(`should return true for '@Component()'`, () => {
+      const decoratorNode = sourceFile.getChildAt(0).getChildAt(1).getChildAt(0).getChildAt(0);
       expect(isComponentDecorator(decoratorNode)).to.be.true;
     });
 
     it(`should return false for other decorator`, () => {
-      const otherDecorator = sourceFile.getChildAt(0).getChildAt(2)
-        .getChildAt(0).getChildAt(0);
+      const otherDecorator = sourceFile.getChildAt(0).getChildAt(1).getChildAt(0).getChildAt(1);
       expect(isComponentDecorator(otherDecorator)).to.be.false;
     });
 
