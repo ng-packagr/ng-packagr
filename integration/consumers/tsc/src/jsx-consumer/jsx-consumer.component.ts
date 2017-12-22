a01bb3ba45063b44dfb0d2cb5226c73be22c2b19
Kyle McNutt <kymcnutt@microsoft.com>
2017-12-22 13:29:35 -0800

    test: fix jsx consumer in integration tests

--- integration/consumers/tsc/src/jsx-consumer/jsx-consumer.component.ts
+++ integration/consumers/tsc/src/jsx-consumer/jsx-consumer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jsx-consumer',
   template: `
-<react-integration-test></react-integration-test>
-`
+    <react-integration-test></react-integration-test>
+  `
 })
 export class JsxConsumerComponent {}
