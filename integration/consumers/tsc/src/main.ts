e1fb56a40c3e535ae01744f8a0c203c34d7ba81d
Kyle McNutt <kymcnutt@microsoft.com>
2017-12-22 13:29:35 -0800

    test: fix jsx consumer in integration tests

--- integration/consumers/tsc/src/main.ts
+++ integration/consumers/tsc/src/main.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { FooComponent, BarComponent } from 'sample-custom';
 import { CoreConsumerModule } from './core-consumer/core-consumer.module';
 import { CustomConsulerModule } from './custom-consumer/custom-consumer.module';
-//import { JsxConsumerModule } from './jsx-consumer/jsx-consumer.module';
+import { JsxConsumerModule } from './jsx-consumer/jsx-consumer.module';
 import { MaterialConsumerModule } from './material-consumer/material-consumer.module';
 import { ModuleImportDirective } from './module-imports';
 
@@ -10,7 +10,7 @@ import { ModuleImportDirective } from './module-imports';
   imports: [
     CoreConsumerModule,
     CustomConsulerModule,
-    //JsxConsumerModule,
+    JsxConsumerModule,
     MaterialConsumerModule
   ],
   declarations: [
