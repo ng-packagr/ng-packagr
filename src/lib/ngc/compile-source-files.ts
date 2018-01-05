bcd55d2117d08127e8948e74a3f203178f2872ee
David <david@spektrakel.de>
2018-01-05 22:16:27 +0100

    fix: redirect tsc and ngc output to `outDir`
    Capture output, including triple-slash referenced typings, in a custom writeFile callback. By setting compilerOptions.outDir equal to compilerOptions.basePath in tsconfig, ensures that triple-slash references have the correct relative path after redirecting output.

--- src/lib/ngc/compile-source-files.ts
+++ src/lib/ngc/compile-source-files.ts
@@ -1,18 +1,29 @@
+import * as fs from 'fs-extra';
 import * as ng from '@angular/compiler-cli';
 import * as ts from 'typescript';
 import * as path from 'path';
 import { createCompilerHostForSynthesizedSourceFiles } from '../ts/synthesized-compiler-host';
+import { redirectWriteFileCompilerHost } from '../ts/redirect-write-file-compiler-host';
 import { TsConfig } from '../ts/tsconfig';
 import * as log from '../util/log';
 import { createEmitCallback } from './create-emit-callback';
 
-export async function compileSourceFiles(sourceFiles: ts.SourceFile[], tsConfig: TsConfig) {
+export async function compileSourceFiles(sourceFiles: ts.SourceFile[], tsConfig: TsConfig, outDir?: string) {
   log.debug(`ngc (v${ng.VERSION.full})`);
+
+  // ts.CompilerHost
+  let tsCompilerHost = createCompilerHostForSynthesizedSourceFiles(sourceFiles, tsConfig.options);
+  if (typeof outDir === 'string' && outDir.length) {
+    // Redirect file output
+    tsCompilerHost = redirectWriteFileCompilerHost(tsCompilerHost, tsConfig.options.basePath, outDir);
+  } else {
+    outDir = tsConfig.options.outDir;
+  }
 
   // ng.CompilerHost
   const ngCompilerHost = ng.createCompilerHost({
     options: tsConfig.options,
-    tsHost: createCompilerHostForSynthesizedSourceFiles(sourceFiles, tsConfig.options)
+    tsHost: tsCompilerHost
   });
 
   // ng.Program
@@ -32,16 +43,24 @@ export async function compileSourceFiles(sourceFiles: ts.SourceFile[], tsConfig:
     oldProgram: ngProgram
   });
 
+  const flatModuleFile = tsConfig.options.flatModuleOutFile;
+  const flatModuleFileExtension = path.extname(flatModuleFile);
+
+  // XX(hack): redirect the `*.metadata.json` to the correct outDir
+  // @link https://github.com/angular/angular/pull/21787
+  const metadataBundleFile = flatModuleFile.replace(flatModuleFileExtension, '.metadata.json');
+  const metadataSrc = path.resolve(tsConfig.options.outDir, metadataBundleFile);
+  const metadataDest = path.resolve(outDir, metadataBundleFile);
+  if (metadataDest !== metadataSrc && fs.existsSync(metadataSrc)) {
+    await fs.move(metadataSrc, metadataDest);
+  }
+
   const exitCode = ng.exitCodeFromResult(result.diagnostics);
   if (exitCode === 0) {
-    const outDir = tsConfig.options.outDir;
-    const outFile = tsConfig.options.flatModuleOutFile;
-    const extName = path.extname(outFile);
-
     return Promise.resolve({
-      js: path.resolve(outDir, outFile),
-      metadata: path.resolve(outDir, outFile.replace(extName, '.metadata.json')),
-      typings: path.resolve(outDir, outFile.replace(extName, '.d.ts'))
+      js: path.resolve(outDir, flatModuleFile),
+      metadata: metadataDest,
+      typings: path.resolve(outDir, flatModuleFile.replace(flatModuleFileExtension, '.d.ts'))
     });
   } else {
     return Promise.reject(new Error(ng.formatDiagnostics(result.diagnostics)));
