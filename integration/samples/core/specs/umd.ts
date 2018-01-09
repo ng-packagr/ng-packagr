36eff9a9e6fd4f48abbc897438b5553822062aa5
JoostK <joost.koehoorn@gmail.com>
2018-01-09 20:19:12 +0100

    feat: include downlevelIteration flag for ES5 bundles

--- integration/samples/core/specs/umd.ts
+++ integration/samples/core/specs/umd.ts
@@ -50,5 +50,12 @@ describe(`@sample/core`, () => {
       expect(API.AngularService).to.be.ok;
     });
 
+    it (`should downlevel iteration`, () => {
+      const iterable = function*() { yield* [1, 2, 3]; };
+      const values = API.AngularService.iterableToArray(iterable());
+
+      expect(values).to.deep.equal([1, 2, 3]);
+    });
+
   });
 });
