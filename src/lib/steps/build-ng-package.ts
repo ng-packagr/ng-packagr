bbc86ddf8b028c40a63a88344b0e4709b1a75cc9
David <david@spektrakel.de>
2018-01-20 17:16:44 +0100

    feat: enable tsconfig customization thru the programmatic API
    Closes #256

--- src/lib/steps/build-ng-package.ts
+++ src/lib/steps/build-ng-package.ts
@@ -1,3 +1,4 @@
+import { InjectionToken, FactoryProvider } from 'injection-js';
 import * as path from 'path';
 import { CliArguments } from '../commands/build.command';
 import { NgArtefacts } from '../ng-package-format/artefacts';
@@ -5,44 +6,59 @@ import { NgPackage } from '../ng-package-format/package';
 import { copyFiles } from '../util/copy';
 import * as log from '../util/log';
 import { rimraf } from '../util/rimraf';
+import { BuildStep } from '../deprecations';
 import { discoverPackages } from './init';
-import { transformSources } from './entry-point-transforms';
+import { ENTRY_POINT_TRANSFORMS_TOKEN } from './entry-point-transforms';
 
 // XX: should eventually become a BuildStep
-export async function buildNgPackage(opts: CliArguments): Promise<void> {
-  log.info(`Building Angular Package`);
+export function buildNgPackageFactory(entryPointTransforms: BuildStep) {
 
-  let ngPackage: NgPackage;
-  try {
-    // READ `NgPackage` from either 'package.json', 'ng-package.json', or 'ng-package.js'
-    ngPackage = await discoverPackages(opts);
+  return async function buildNgPackage(opts: CliArguments): Promise<void> {
+    log.info(`Building Angular Package`);
 
-    // clean the primary dest folder (should clean all secondary module directories as well)
-    await rimraf(ngPackage.dest);
+    let ngPackage: NgPackage;
+    try {
+      // READ `NgPackage` from either 'package.json', 'ng-package.json', or 'ng-package.js'
+      ngPackage = await discoverPackages(opts);
 
-    const artefacts = new NgArtefacts(ngPackage.primary, ngPackage);
-    await transformSources({ artefacts, entryPoint: ngPackage.primary, pkg: ngPackage });
-    for (const secondary of ngPackage.secondaries) {
-      const artefacts = new NgArtefacts(secondary, ngPackage);
-      await transformSources({ artefacts, entryPoint: secondary, pkg: ngPackage });
+      // clean the primary dest folder (should clean all secondary module directories as well)
+      await rimraf(ngPackage.dest);
+
+      // Sequentially build entry points
+      const entryPoints = [ ngPackage.primary, ...ngPackage.secondaries ];
+      for (const entryPoint of entryPoints) {
+        // Prepare artefacts. Will be populated by the entry point transformations
+        const artefacts = new NgArtefacts(ngPackage.primary, ngPackage);
+        await entryPointTransforms({ artefacts, entryPoint, pkg: ngPackage });
+      }
+
+      await copyFiles(`${ngPackage.src}/README.md`, ngPackage.dest);
+      await copyFiles(`${ngPackage.src}/LICENSE`, ngPackage.dest);
+
+      // clean the working directory for a successful build only
+      await rimraf(ngPackage.workingDirectory);
+      log.success(`Built Angular Package!
+   - from: ${ngPackage.src}
+   - to:   ${ngPackage.dest}
+      `);
+    } catch (error) {
+      // Report error messages and throw the error further up
+      log.error(error);
+      if (ngPackage) {
+        log.info(`Build failed. The working directory was not pruned. Files are stored at ${ngPackage.workingDirectory}.`);
+      }
+
+      throw error;
     }
-
-    await copyFiles(`${ngPackage.src}/README.md`, ngPackage.dest);
-    await copyFiles(`${ngPackage.src}/LICENSE`, ngPackage.dest);
-
-    // clean the working directory for a successful build only
-    await rimraf(ngPackage.workingDirectory);
-    log.success(`Built Angular Package!
- - from: ${ngPackage.src}
- - to:   ${ngPackage.dest}
-    `);
-  } catch (error) {
-    // Report error messages and throw the error further up
-    log.error(error);
-    if (ngPackage) {
-      log.info(`Build failed. The working directory was not pruned. Files are stored at ${ngPackage.workingDirectory}.`);
-    }
-
-    throw error;
-  }
+  };
 }
+
+export type BuildCallSignature = (opts: CliArguments) => Promise<void>;
+
+export const BUILD_NG_PACKAGE_TOKEN = new InjectionToken<BuildCallSignature>('ng.v5.buildNgPackage');
+
+export const BUILD_NG_PACKAGE_PROVIDER: FactoryProvider = {
+  provide: BUILD_NG_PACKAGE_TOKEN,
+  useFactory: buildNgPackageFactory,
+  deps: [ ENTRY_POINT_TRANSFORMS_TOKEN ]
+};
