b0a637723dc376bed9bbad5307b1369d5168f63c
David <david@spektrakel.de>
2018-01-27 15:15:25 +0100

    test: cover programmatic api surface

--- src/lib/ng-v5/packagr.spec.ts
+++ src/lib/ng-v5/packagr.spec.ts
@@ -4,30 +4,45 @@ import { provideProject, PROJECT_TOKEN, ngPackagr, NgPackagr } from './packagr';
 import { DEFAULT_TS_CONFIG_TOKEN } from '../ts/default-tsconfig';
 
 describe(`ngPackagr()`, () => {
+  let packager: NgPackagr;
+  beforeEach(() => {
+    packager = ngPackagr();
+  });
   it(`should return a NgPackagr instance`, () => {
-    const foo = ngPackagr();
-    expect(foo).to.be.an.instanceOf(NgPackagr);
+    expect(packager).to.be.an.instanceOf(NgPackagr);
   });
 
   it(`should have a default tsconfig`, () => {
-    const toBeTested = ngPackagr();
-    const defaultTsConfigProvider = toBeTested['providers'].filter(p => (p as any).provide === DEFAULT_TS_CONFIG_TOKEN);
+    const defaultTsConfigProvider = packager['providers'].filter(p => (p as any).provide === DEFAULT_TS_CONFIG_TOKEN);
     expect(defaultTsConfigProvider).to.have.length(1);
+  });
+
+  describe(`forProject()`, () => {
+    it(`should return self instance for chaining`, () => {
+      expect(packager.forProject('foo')).to.equal(packager);
+    });
+
+    it(`should set project provider`, () => {
+      const providers = packager.forProject('foobar')['providers'].filter(p => (p as any).provide === PROJECT_TOKEN);
+
+      expect(providers).to.have.length(1);
+      expect((providers[0] as any).useValue).to.equal('foobar');
+    });
   });
 
   describe(`withTsConfig()`, () => {
     it(`should return self instance for chaining`, () => {
-      const toBeTested = ngPackagr();
       const mockConfig = ({ project: 'foo' } as any) as ng.ParsedConfiguration;
-      expect(toBeTested.withTsConfig(mockConfig)).to.equal(toBeTested);
+      expect(packager.withTsConfig(mockConfig)).to.equal(packager);
     });
     it(`should override the default tsconfig provider`, () => {
       const mockConfig = ({ project: 'foo' } as any) as ng.ParsedConfiguration;
-      const toBeTested = ngPackagr().withTsConfig(mockConfig);
-      const tsConfigProviders = toBeTested['providers'].filter(p => (p as any).provide === DEFAULT_TS_CONFIG_TOKEN);
+      const providers = packager
+        .withTsConfig(mockConfig)
+        ['providers'].filter(p => (p as any).provide === DEFAULT_TS_CONFIG_TOKEN);
 
-      expect(tsConfigProviders).to.have.length(2);
-      expect((tsConfigProviders[1] as any).useValue).to.be.satisfy(val => val.project === 'foo');
+      expect(providers).to.have.length(2);
+      expect((providers[1] as any).useValue).to.satisfy(val => val.project === 'foo');
     });
   });
 });
