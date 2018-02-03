0cb0cce2c241e38b4228bfeb6c700dd0ec3ebd72
David Herges <dherges@users.noreply.github.com>
2018-02-03 09:22:22 +0100

    fix: update rollup to version `^0.55.0` (#534)
    Closes #488, #523

--- src/lib/flatten/flatten.ts
+++ src/lib/flatten/flatten.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import { remapSourceMap } from '../sourcemaps/remap';
 import * as log from '../util/log';
-import { rollup } from './rollup';
+import { rollupBundleFile } from './rollup';
 import { downlevelWithTsc } from './tsc';
 import { minifyJsFile } from './uglify';
 
@@ -40,7 +40,7 @@ export async function writeFlatBundleFiles(opts: FlattenOpts) {
 export async function flattenToFesm15(opts: FlattenOpts): Promise<string> {
   const destFile = path.resolve(opts.outDir, 'esm2015', opts.flatModuleFile + '.js');
 
-  await rollup({
+  await rollupBundleFile({
     moduleName: opts.esmModuleId,
     entry: opts.entryFile,
     format: 'es',
@@ -63,7 +63,7 @@ export async function flattenToUmd(opts: FlattenOpts): Promise<string> {
   const destFile = path.resolve(opts.outDir, 'bundles', opts.flatModuleFile + '.umd.js');
   const { embedded = [] } = opts;
 
-  await rollup({
+  await rollupBundleFile({
     moduleName: opts.umdModuleId,
     entry: opts.entryFile,
     format: 'umd',
--- src/lib/flatten/rollup.ts
+++ src/lib/flatten/rollup.ts
@@ -1,4 +1,4 @@
-import * as __rollup from 'rollup';
+import * as rollup from 'rollup';
 import * as nodeResolve from 'rollup-plugin-node-resolve';
 import * as commonJs from 'rollup-plugin-commonjs';
 import * as path from 'path';
@@ -6,34 +6,42 @@ import * as log from '../util/log';
 import { externalModuleIdStrategy } from './external-module-id-strategy';
 import { umdModuleIdStrategy } from './umd-module-id-strategy';
 
-export type BundleFormat = __rollup.Format;
-
+/**
+ * Options used in `ng-packagr` for writing flat bundle files.
+ *
+ * These options are passed through to rollup.
+ */
 export interface RollupOptions {
   moduleName: string;
   entry: string;
-  format: BundleFormat;
+  format: rollup.ModuleFormat;
   dest: string;
   umdModuleIds?: { [key: string]: string };
   embedded?: string[];
 }
 
 /** Runs rollup over the given entry file, writes a bundle file. */
-export async function rollup(opts: RollupOptions): Promise<void> {
-  log.debug(`rollup (v${__rollup.VERSION}) ${opts.entry} to ${opts.dest} (${opts.format})`);
+export async function rollupBundleFile(opts: RollupOptions): Promise<void> {
+  log.debug(`rollup (v${rollup.VERSION}) ${opts.entry} to ${opts.dest} (${opts.format})`);
 
   // Create the bundle
-  const bundle: __rollup.Bundle = await __rollup.rollup({
+  const bundle: rollup.OutputChunk = await rollup.rollup({
     context: 'this',
     external: moduleId => externalModuleIdStrategy(moduleId, opts.embedded || []),
     input: opts.entry,
     plugins: [nodeResolve({ jsnext: true, module: true }), commonJs()],
     onwarn: warning => {
-      if (warning.code === 'THIS_IS_UNDEFINED') {
-        return;
-      }
+      if (typeof warning === 'string') {
+        log.warn(warning);
+      } else {
+        if (warning.code === 'THIS_IS_UNDEFINED') {
+          return;
+        }
 
-      log.warn(warning.message);
-    }
+        log.warn(warning.message);
+      }
+    },
+    preserveSymlinks: true
   });
 
   // Output the bundle to disk
