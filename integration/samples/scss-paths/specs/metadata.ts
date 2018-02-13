ab973f401589111116f3a2c2ca6a780e34b25c3a
Michael Wooten <mwooten.dev@gmail.com>
2018-02-13 13:10:30 -0500

    feat: `styleIncludePaths` for feature parity with Angular CLI (#603)
    Closes #282

--- integration/samples/scss-paths/specs/metadata.ts
+++ integration/samples/scss-paths/specs/metadata.ts
@@ -1,7 +1,6 @@
 import { expect } from 'chai';
 
 describe(`@sample/scss-paths`, () => {
-
   describe(`sample-scss-paths.metadata.json`, () => {
     let METADATA;
     before(() => {
@@ -16,10 +15,21 @@ describe(`@sample/scss-paths`, () => {
       expect(METADATA['importAs']).to.equal('@sample/scss-paths');
     });
 
-    it(`should resolve the styles from the theme`, () => {
-      const styles = METADATA['metadata']['BazComponent']['decorators'][0]['arguments'][0]['styles'][0];
-      expect(styles).to.contain(`color:"red"`);
-      expect(styles).to.contain(`background-color:"yellow"`);
+    it(`should resolve the styles from the SCSS theme`, () => {
+      const scssStyles = METADATA['metadata']['BazComponent']['decorators'][0]['arguments'][0]['styles'][0];
+      expect(scssStyles).to.contain(`color:"red"`);
+      expect(scssStyles).to.contain(`background-color:"yellow"`);
+    });
+
+    it(`should resolve the styles from the Less theme`, () => {
+      const lessStyles = METADATA['metadata']['BazComponent']['decorators'][0]['arguments'][0]['styles'][1];
+      expect(lessStyles).to.contain(`.baz .oom`);
+      expect(lessStyles).to.contain(`color:'red'`);
+    });
+
+    it(`should resolve the styles from the Stylus theme`, () => {
+      const stylusStyles = METADATA['metadata']['BazComponent']['decorators'][0]['arguments'][0]['styles'][2];
+      expect(stylusStyles).to.contain(`font-size:32pt`);
     });
   });
 
@@ -37,14 +47,34 @@ describe(`@sample/scss-paths`, () => {
       expect(METADATA['importAs']).to.equal('@sample/scss-paths/sub-module');
     });
 
-    it(`should resolve the styles from the parent theme`, () => {
-      const styles = METADATA['metadata']['BarComponent']['decorators'][0]['arguments'][0]['styles'][0];
-      expect(styles).to.contain(`background-color:"yellow"`);
+    it(`should resolve the SCSS styles from the parent theme`, () => {
+      const scssStyles = METADATA['metadata']['BarComponent']['decorators'][0]['arguments'][0]['styles'][0];
+      expect(scssStyles).to.contain(`background-color:"yellow"`);
     });
 
-    it(`should resolve the styles from the sub-module common utilities`, () => {
-      const styles = METADATA['metadata']['BarComponent']['decorators'][0]['arguments'][0]['styles'][0];
-      expect(styles).to.contain(`border:10px solid "yellow"`);
+    it(`should resolve the SCSS styles from the sub-module common utilities`, () => {
+      const scssStyles = METADATA['metadata']['BarComponent']['decorators'][0]['arguments'][0]['styles'][0];
+      expect(scssStyles).to.contain(`border:10px solid "yellow"`);
+    });
+
+    it(`should resolve the Less styles from the parent theme`, () => {
+      const lessStyles = METADATA['metadata']['BarComponent']['decorators'][0]['arguments'][0]['styles'][1];
+      expect(lessStyles).to.contain(`color:'red'`);
+    });
+
+    it(`should resolve the Less styles from the sub-module common utilities`, () => {
+      const lessStyles = METADATA['metadata']['BarComponent']['decorators'][0]['arguments'][0]['styles'][1];
+      expect(lessStyles).to.contain(`font-weight:bold`);
+    });
+
+    it(`should resolve the Stylus styles from the parent theme`, () => {
+      const stylusStyles = METADATA['metadata']['BarComponent']['decorators'][0]['arguments'][0]['styles'][2];
+      expect(stylusStyles).to.contain(`font-size:32pt`);
+    });
+
+    it(`should resolve the Stylus styles from the sub-module common utilities`, () => {
+      const stylusStyles = METADATA['metadata']['BarComponent']['decorators'][0]['arguments'][0]['styles'][2];
+      expect(stylusStyles).to.contain(`font-face:sans-serif`);
     });
   });
 });
