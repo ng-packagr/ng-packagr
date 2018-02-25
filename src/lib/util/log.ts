cea714bcf42703216d6b424eb56c301b0a386dfe
Alan Agius <alan.agius4@gmail.com>
2018-02-25 11:45:31 +0100

    fix: cannot find module `chalk`
    Added `chalk` as `dependency`
    
    Closes: #646

--- src/lib/util/log.ts
+++ src/lib/util/log.ts
@@ -1,7 +1,6 @@
-const chalk = require('chalk');
+import chalk from 'chalk';
 
 export const error = (err: string | Error) => {
-
   if (err instanceof Error) {
     console.log('\n' + chalk.red('BUILD ERROR'));
     console.log(chalk.red(err.message));
@@ -9,25 +8,22 @@ export const error = (err: string | Error) => {
   } else {
     console.log(chalk.red(err));
   }
-
-}
+};
 
 export const warn = (msg: string) => {
   console.log(chalk.yellow(msg));
-}
+};
 
 export const success = (msg: string) => {
   console.log(chalk.green(msg));
-}
+};
 
 export const info = (msg: string) => {
   console.log(chalk.blue(msg));
-}
+};
 
 export const debug = (msg: string) => {
-
   if (process.env.DEBUG) {
     console.log(chalk.inverse.cyan(`[debug] ${msg}`));
   }
-
-}
+};
