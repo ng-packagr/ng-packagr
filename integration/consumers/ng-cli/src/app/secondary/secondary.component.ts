410721baba5b3b90545d903ab07353ab9983522b
David <david@spektrakel.de>
2018-03-11 18:27:07 +0100

    test: build CLI consumer app depending on @sample/secondary

---
+++ integration/consumers/ng-cli/src/app/secondary/secondary.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { SHARED_FEATURE } from '@sample/secondary/shared';
+import { FEATURE_A } from '@sample/secondary/feature-a';
+import { FEATURE_B } from '@sample/secondary/feature-b';
+
+export const YEAH_I_AM_RUNNING_IN_A_LOOP_FAST = [SHARED_FEATURE, FEATURE_A, FEATURE_B];
+
+@Component({
+  selector: 'app-secondary',
+  template: `
+    <ul>
+      <li *ngFor="let string of strings">{{ string }}</li>
+    </ul>
+  `
+})
+export class SecondaryComponent {
+  strings = YEAH_I_AM_RUNNING_IN_A_LOOP_FAST;
+}
