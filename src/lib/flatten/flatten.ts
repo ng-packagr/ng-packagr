1b7d9679aad6d995ed4671024933e1f40bf42df5
David <david@spektrakel.de>
2018-03-11 17:45:51 +0100

    fix: write umd id of entry point dependency to umd bundle

--- src/lib/flatten/flatten.ts
+++ src/lib/flatten/flatten.ts
@@ -9,17 +9,22 @@ export interface FlattenOpts {
   entryFile: string;
   outDir: string;
   flatModuleFile: string;
+
+  /** ECMAScript module ID defined by the FESM bundles. */
   esmModuleId: string;
+
+  /** UMD ID defined by the UMD bundle. */
   umdModuleId: string;
+
+  /** AMD ID defined in the UMD bundle. */
+  amdId?: string;
   comments: string;
   licensePath: string;
 
-  /** List of module ids that should be embedded to the bundle. */
+  /** List of module IDs that should be embedded to the bundle (embedded dependencies). */
   embedded?: string[];
 
-  amdId?: string;
-
-  /** Map of external UMD module ids that  */
+  /** Map of external UMD module IDs (dependencies).  */
   umdModuleIds?: { [key: string]: string };
 }
 
--- src/lib/ng-v5/entry-point/write-bundles.transform.ts
+++ src/lib/ng-v5/entry-point/write-bundles.transform.ts
@@ -7,12 +7,22 @@ import { Node } from '../../brocc/node';
 import { Transform } from '../../brocc/transform';
 import { writeFlatBundleFiles, FlattenOpts } from '../../flatten/flatten';
 import { NgEntryPoint } from '../../ng-package-format/entry-point';
-import { isEntryPointInProgress } from '../nodes';
+import { isEntryPoint, isEntryPointInProgress } from '../nodes';
 
 export const writeBundlesTransform: Transform = pipe(
   switchMap(graph => {
     const entryPoint = graph.find(isEntryPointInProgress());
     const ngEntryPoint: NgEntryPoint = entryPoint.data.entryPoint;
+
+    // Add UMD module IDs for dependencies
+    const dependencyUmdIds = entryPoint
+      .filter(isEntryPoint)
+      .map(ep => ep.data.entryPoint)
+      .reduce((prev, ep: NgEntryPoint) => {
+        prev[ep.moduleId] = ep.umdId;
+
+        return prev;
+      }, {});
 
     const opts: FlattenOpts = {
       entryFile: entryPoint.data.es2015EntryFile,
@@ -20,11 +30,14 @@ export const writeBundlesTransform: Transform = pipe(
       flatModuleFile: ngEntryPoint.flatModuleFile,
       esmModuleId: ngEntryPoint.moduleId,
       umdModuleId: ngEntryPoint.umdId,
-      umdModuleIds: ngEntryPoint.umdModuleIds,
+      amdId: ngEntryPoint.amdId,
+      umdModuleIds: {
+        ...ngEntryPoint.umdModuleIds,
+        ...dependencyUmdIds
+      },
       embedded: ngEntryPoint.embedded,
       comments: ngEntryPoint.comments,
-      licensePath: ngEntryPoint.licensePath,
-      amdId: ngEntryPoint.amdId
+      licensePath: ngEntryPoint.licensePath
     };
 
     return fromPromise(writeFlatBundleFiles(opts)).pipe(map(() => graph));
