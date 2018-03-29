b289d363e0fe8e646e97180b6d951d09c5edd3ef
Alan Agius <alan.agius4@gmail.com>
2018-03-29 19:00:14 +0200

    test: remove tests which are no longer needed

--- integration/samples/comments/public_api.ts
+++
@@ -1,5 +0,0 @@
-export * from './src/angular.component';
-export * from './src/angular.directive';
-export * from './src/angular.module';
-export * from './src/angular.pipe';
-export * from './src/angular.service';
