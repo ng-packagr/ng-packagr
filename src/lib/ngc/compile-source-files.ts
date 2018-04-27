f61e54848074fab889d033f36c15967ca4c30642
Alan Agius <alan.agius4@gmail.com>
2018-04-27 14:00:02 +0200

    fix: don't pass `emitFlags` leave it up to `ngc`

--- src/lib/ngc/compile-source-files.ts
+++ src/lib/ngc/compile-source-files.ts
@@ -44,7 +44,6 @@ export async function compileSourceFiles(
   const result = ng.performCompilation({
     rootNames: tsConfig.rootNames,
     options: tsConfigOptions,
-    emitFlags: tsConfig.emitFlags,
     emitCallback: createEmitCallback(tsConfigOptions),
     host: ngCompilerHost,
     oldProgram: ngProgram
