9bd0c72182391a8c9576b87adcdc76339d5e3749
Alan Agius <alan.agius4@gmail.com>
2018-05-08 14:38:34 +0200

    fix: handle nested entry points with same name
    Using `basepath` causes an issue that nested entrypoints will lose their full path, which might also cause accidental override of modules if they are named the same
    
    Closes: #851, #849

--- integration/samples/apf/specs/files.ts
+++ integration/samples/apf/specs/files.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import * as glob from 'glob';
 import * as fs from 'fs';
 
-describe(`@sample/apf`, () => {
+describe('@sample/apf', () => {
   let DIST: string;
   before(() => {
     DIST = path.resolve(__dirname, '../dist');
@@ -16,98 +16,112 @@ describe(`@sample/apf`, () => {
     });
   });
 
-  describe(`FESM2015`, () => {
-    it(`should contain 2 '.js.map' files`, () => {
-      expect(glob.sync(`${DIST}/fesm2015/**/*.js.map`).length).equal(2);
-    });
-
-    it(`should contain 2 '.js' files`, () => {
-      expect(glob.sync(`${DIST}/fesm2015/**/*.js`).length).equal(2);
-    });
-  });
-
-  describe(`FESM5`, () => {
-    it(`should contain 2 '.js.map' files`, () => {
-      expect(glob.sync(`${DIST}/fesm5/**/*.js.map`).length).equal(2);
-    });
-
-    it(`should contain 2 '.js' files`, () => {
-      expect(glob.sync(`${DIST}/fesm5/**/*.js`).length).equal(2);
-    });
-  });
-
-  describe(`UMD`, () => {
+  describe('FESM2015', () => {
     it(`should contain 4 '.js.map' files`, () => {
-      expect(glob.sync(`${DIST}/bundles/**/*.js.map`).length).equal(4);
+      expect(glob.sync(`${DIST}/fesm2015/**/*.js.map`).length).equal(4);
     });
 
     it(`should contain 4 '.js' files`, () => {
-      expect(glob.sync(`${DIST}/bundles/**/*.js`).length).equal(4);
+      expect(glob.sync(`${DIST}/fesm2015/**/*.js`).length).equal(4);
     });
   });
 
-  describe(`ESM5`, () => {
+  describe('FESM5', () => {
+    it(`should contain 4 '.js.map' files`, () => {
+      expect(glob.sync(`${DIST}/fesm5/**/*.js.map`).length).equal(4);
+    });
+
+    it(`should contain 4 '.js' files`, () => {
+      expect(glob.sync(`${DIST}/fesm5/**/*.js`).length).equal(4);
+    });
+  });
+
+  describe('UMD', () => {
     it(`should contain 8 '.js.map' files`, () => {
-      expect(glob.sync(`${DIST}/esm5/**/*.js.map`).length).equal(8);
+      expect(glob.sync(`${DIST}/bundles/**/*.js.map`).length).equal(8);
     });
 
     it(`should contain 8 '.js' files`, () => {
-      expect(glob.sync(`${DIST}/esm5/**/*.js`).length).equal(8);
+      expect(glob.sync(`${DIST}/bundles/**/*.js`).length).equal(8);
+    });
+  });
+
+  describe('ESM5', () => {
+    it(`should contain 16 '.js.map' files`, () => {
+      expect(glob.sync(`${DIST}/esm5/**/*.js.map`).length).equal(16);
     });
 
-    describe(`secondary`, () => {
-      it(`should contain 4 '.js.map' files`, () => {
-        expect(glob.sync(`${DIST}/esm5/secondary/**/*.js.map`).length).equal(4);
+    it(`should contain 16 '.js' files`, () => {
+      expect(glob.sync(`${DIST}/esm5/**/*.js`).length).equal(16);
+    });
+
+    describe('secondary', () => {
+      it(`should contain 8 '.js.map' files`, () => {
+        expect(glob.sync(`${DIST}/esm5/secondary/**/*.js.map`).length).equal(8);
       });
 
-      it(`should contain 4 '.js' files`, () => {
-        expect(glob.sync(`${DIST}/esm5/secondary/**/*.js`).length).equal(4);
+      it(`should contain 8 '.js' files`, () => {
+        expect(glob.sync(`${DIST}/esm5/secondary/**/*.js`).length).equal(8);
       });
     });
   });
 
-  describe(`ESM2015`, () => {
-    it(`should contain 8 '.js.map' files`, () => {
-      expect(glob.sync(`${DIST}/esm2015/**/*.js.map`).length).equal(8);
+  describe('ESM2015', () => {
+    it(`should contain 16 '.js.map' files`, () => {
+      expect(glob.sync(`${DIST}/esm2015/**/*.js.map`).length).equal(16);
     });
 
-    it(`should contain 8 '.js' files`, () => {
-      expect(glob.sync(`${DIST}/esm2015/**/*.js`).length).equal(8);
+    it(`should contain 16 '.js' files`, () => {
+      expect(glob.sync(`${DIST}/esm2015/**/*.js`).length).equal(16);
     });
 
-    describe(`secondary`, () => {
-      it(`should contain 4 '.js.map' files`, () => {
-        expect(glob.sync(`${DIST}/esm2015/secondary/**/*.js.map`).length).equal(4);
+    describe('secondary', () => {
+      it(`should contain 8 '.js.map' files`, () => {
+        expect(glob.sync(`${DIST}/esm2015/secondary/**/*.js.map`).length).equal(8);
       });
 
-      it(`should contain 4 '.js' files`, () => {
-        expect(glob.sync(`${DIST}/esm2015/secondary/**/*.js`).length).equal(4);
+      it(`should contain 8 '.js' files`, () => {
+        expect(glob.sync(`${DIST}/esm2015/secondary/**/*.js`).length).equal(8);
       });
     });
   });
 
-  describe(`sample-apf.metadata.json`, () => {
+  describe('sample-apf.metadata.json', () => {
     it(`should exist`, () => {
       const file = fs.existsSync(path.join(DIST, 'sample-apf.metadata.json'));
       expect(file).to.be.true;
     });
   });
 
-  describe(`sample-apf-secondary.metadata.json`, () => {
+  describe('sample-apf-secondary.metadata.json', () => {
     it(`should exist`, () => {
       const file = fs.existsSync(path.join(DIST, 'secondary', 'sample-apf-secondary.metadata.json'));
       expect(file).to.be.true;
     });
   });
 
-  describe(`sample-apf.d.ts`, () => {
+  describe('sample-apf-secondary-testing.metadata.json', () => {
+    it(`should exist`, () => {
+      const file = fs.existsSync(path.join(DIST, 'secondary', 'testing', 'sample-apf-secondary-testing.metadata.json'));
+      expect(file).to.be.true;
+    });
+  });
+
+  describe('sample-apf-testing.metadata.json', () => {
+    it(`should exist`, () => {
+      const file = fs.existsSync(path.join(DIST, 'testing', 'sample-apf-testing.metadata.json'));
+      expect(file).to.be.true;
+    });
+  });
+
+  describe('sample-apf.d.ts', () => {
     it(`should exist`, () => {
       const file = fs.existsSync(path.join(DIST, 'sample-apf.d.ts'));
       expect(file).to.be.true;
     });
   });
 
-  describe(`sample-apf-secondary.d.ts`, () => {
+  describe('sample-apf-secondary.d.ts', () => {
     it(`should exist`, () => {
       const file = fs.existsSync(path.join(DIST, 'secondary', 'sample-apf-secondary.d.ts'));
       expect(file).to.be.true;
