811b9a05c85a3cdcaeeded9db1bc0eecd44f1481
Alan Agius <alan.agius4@gmail.com>
2018-05-09 08:53:08 +0200

    fix: add `sourceMappingURL` to rollup bundle

--- src/lib/flatten/rollup.ts
+++ src/lib/flatten/rollup.ts
@@ -2,6 +2,7 @@ import * as rollup from 'rollup';
 import * as nodeResolve from 'rollup-plugin-node-resolve';
 import * as sourcemaps from 'rollup-plugin-sourcemaps';
 import * as commonJs from 'rollup-plugin-commonjs';
+import * as path from 'path';
 import * as log from '../util/log';
 import { ExternalModuleIdStrategy, DependencyList } from './external-module-id-strategy';
 import { umdModuleIdStrategy } from './umd-module-id-strategy';
@@ -52,13 +53,17 @@ export async function rollupBundleFile(opts: RollupOptions): Promise<void[]> {
   });
 
   // Output the bundle to disk
+  const sourcemapFullFile = `${opts.dest}.map`;
+  const sourcemapFile = path.basename(sourcemapFullFile);
   const result = await bundle.generate({
     name: opts.moduleName,
     format: opts.format,
     amd: opts.amd,
+    file: opts.dest,
     banner: '',
     globals: moduleId => umdModuleIdStrategy(moduleId, opts.umdModuleIds || {}),
-    sourcemap: true
+    sourcemap: true,
+    sourcemapFile
   });
 
   // relocate sourcemaps
@@ -76,5 +81,8 @@ export async function rollupBundleFile(opts: RollupOptions): Promise<void[]> {
     }
   });
 
-  return Promise.all([outputJson(`${opts.dest}.map`, result.map), outputFile(opts.dest, result.code)]);
+  // rollup doesn't add a sourceMappingURL
+  // https://github.com/rollup/rollup/issues/121
+  result.code = `${result.code}//# sourceMappingURL=${sourcemapFile}`;
+  return Promise.all([outputJson(sourcemapFullFile, result.map), outputFile(opts.dest, result.code)]);
 }
