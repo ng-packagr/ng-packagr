53d590cdf4522858c6c5905ff551df8ef1eda3fa
Alan Agius <alan.agius4@gmail.com>
2018-05-11 17:52:28 +0200

    perf: remove extra template type checking for downleveling

--- integration/samples/material/ui-lib.module.ts
+++ integration/samples/material/ui-lib.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FooComponent } from './foo/foo.component';
 import { BarComponent } from './bar/bar.component';
 import { FooBarComponent } from './foo-bar/foo-bar.component';
 import { BazComponent } from './baz/baz.component';
 
 @NgModule({
-  declarations: [
-    FooComponent,
-    BarComponent,
-    FooBarComponent,
-    BazComponent,
-  ],
-  exports: [
-    FooComponent,
-    BarComponent,
-    FooBarComponent,
-    BazComponent,
-  ]
+  imports: [RouterModule.forChild([])],
+  declarations: [FooComponent, BarComponent, FooBarComponent, BazComponent],
+  exports: [FooComponent, BarComponent, FooBarComponent, BazComponent]
 })
 export class UiLibModule {}
