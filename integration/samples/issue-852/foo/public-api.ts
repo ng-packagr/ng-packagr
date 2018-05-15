364728ad0add104cabf78e038d1ad1784a32a2e5
Alan Agius <alan.agius4@gmail.com>
2018-05-15 08:57:16 +0200

    fix: Injector token failure when having dependency like `a -> b -> c`
    Closes: #852

---
+++ integration/samples/issue-852/foo/public-api.ts
@@ -0,0 +1,12 @@
+import { Platform } from '@angular/cdk/platform';
+import { Inject, Injectable, Optional } from '@angular/core';
+import { BAR_TOKEN } from '@my/issue-852/bar';
+
+@Injectable()
+export class FooClass {
+  constructor(
+    @Optional()
+    @Inject(BAR_TOKEN)
+    matDateLocale: string
+  ) {}
+}
--- src/lib/ng-v5/init/init-tsconfig.transform.ts
+++ src/lib/ng-v5/init/init-tsconfig.transform.ts
@@ -2,18 +2,12 @@
 import { ParsedConfiguration } from '@angular/compiler-cli/src/perform_compile';
 import { Transform, transformFromPromise } from '../../brocc/transform';
 import { TsConfig, initializeTsConfig } from '../../ts/tsconfig';
-import * as log from '../../util/log';
-import { isEntryPoint } from '../nodes';
+import { isEntryPoint, EntryPointNode } from '../nodes';
 
 export const initTsConfigTransformFactory = (defaultTsConfig: TsConfig): Transform =>
   transformFromPromise(async graph => {
     // Initialize tsconfig for each entry point
-    const entryPoints = graph.filter(isEntryPoint);
-    for (let entryPoint of entryPoints) {
-      log.debug(`Initializing tsconfig for ${entryPoint.data.entryPoint.moduleId}`);
-      const tsConfig = initializeTsConfig(defaultTsConfig, entryPoint.data.entryPoint);
-      entryPoint.data.tsConfig = tsConfig;
-    }
-
+    const entryPoints = graph.filter(isEntryPoint) as EntryPointNode[];
+    initializeTsConfig(defaultTsConfig, entryPoints);
     return graph;
   });
