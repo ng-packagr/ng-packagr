35a89eb69e0f32e6068def74a53691a9a2ccce21
David <david@spektrakel.de>
2018-06-08 23:54:38 +0200

    fix: remove dependency cpx

--- src/lib/util/copy.ts
+++ src/lib/util/copy.ts
@@ -1,19 +1,14 @@
-import { copy, AsyncOptions } from 'cpx';
-import { promisify } from './promisify';
+import * as fs from 'fs-extra';
 import { debug } from './log';
 import { toArray } from './array';
 
-export async function copyFiles(src: string | string[], dest: string, options?: AsyncOptions): Promise<void[]> {
-  const promises = toArray(src).map(src => {
-    debug(`copyFiles from ${src} to ${dest}`);
-    return promisify<void>(resolveOrReject => {
-      if (options) {
-        copy(src, dest, options, resolveOrReject);
-      } else {
-        copy(src, dest, resolveOrReject);
-      }
-    });
-  });
-
-  return Promise.all(promises);
+export async function copyFiles(src: string | string[], dest: string): Promise<void> {
+  const files = toArray(src);
+  for (let file of files) {
+    const doesExist = await fs.pathExists(file);
+    if (doesExist) {
+      debug(`copyFiles from ${src} to ${dest}`);
+      await fs.copy(file, dest);
+    }
+  }
 }
