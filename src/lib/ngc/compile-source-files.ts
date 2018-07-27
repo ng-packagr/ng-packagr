1296be5d57a4a10ed8eaa445d2ce06ebfca56c4c
Alan <alan.agius4@gmail.com>
2018-07-27 09:28:58 +0200

    refactor: better tsconfig defaults and remove code for rewiring
    With this change consumers will be able to remove the below from their tsconfig, and now we can still have the correct emit flags
    
    ```
    "flatModuleId": "AUTOGENERATED",
    "flatModuleOutFile": "AUTOGENERATED",
    ```

--- src/lib/ngc/compile-source-files.ts
+++ src/lib/ngc/compile-source-files.ts
@@ -1,7 +1,5 @@
-import * as fs from 'fs-extra';
 import * as ng from '@angular/compiler-cli';
 import * as ts from 'typescript';
-import * as path from 'path';
 import { TsConfig } from '../ts/tsconfig';
 import * as log from '../util/log';
 import { createEmitCallback } from './create-emit-callback';
@@ -35,26 +33,15 @@ export async function compileSourceFiles(
 
   // Don't use `ng.emit` as it doesn't output all errors.
   // https://github.com/angular/angular/issues/24024
+  const emitFlags = tsConfigOptions.declaration ? tsConfig.emitFlags : ng.EmitFlags.JS;
+
   const result = ng.performCompilation({
     rootNames: tsConfig.rootNames,
     options: tsConfigOptions,
     host: ngCompilerHost,
     emitCallback: createEmitCallback(tsConfigOptions),
-    emitFlags: tsConfig.emitFlags
+    emitFlags
   });
-
-  // XX(hack): redirect the `*.metadata.json` to the correct outDir
-  // @link https://github.com/angular/angular/pull/21787
-  if (declarationDir) {
-    const flatModuleFile = tsConfigOptions.flatModuleOutFile;
-    const flatModuleFileExtension = path.extname(flatModuleFile);
-    const metadataBundleFile = flatModuleFile.replace(flatModuleFileExtension, '.metadata.json');
-    const metadataSrc = path.resolve(tsConfigOptions.declarationDir, metadataBundleFile);
-    const metadataDest = path.resolve(declarationDir, metadataBundleFile);
-    if (metadataDest !== metadataSrc && fs.existsSync(metadataSrc)) {
-      fs.moveSync(metadataSrc, metadataDest, { overwrite: true });
-    }
-  }
 
   const exitCode = ng.exitCodeFromResult(result.diagnostics);
   return exitCode === 0 ? Promise.resolve() : Promise.reject(new Error(ng.formatDiagnostics(result.diagnostics)));
