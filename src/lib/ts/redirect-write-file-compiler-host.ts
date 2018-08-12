52d9a57215b7b7c34e7c3f2b7a6e8c8c557e9bd4
Alan Agius <alan.agius4@gmail.com>
2018-08-12 13:47:19 +0200

    refactor: better tsconfig defaults and remove code for rewiring (#1024)

--- src/lib/ts/redirect-write-file-compiler-host.ts
+++ src/lib/ts/redirect-write-file-compiler-host.ts
@@ -23,7 +23,7 @@ export function redirectWriteFileCompilerHost(
       sourceFiles?: ReadonlyArray<ts.SourceFile>
     ) => {
       let filePath = fileName;
-      if (fileName.endsWith('.d.ts')) {
+      if (/(\.d\.ts|\.metadata\.json)$/.test(fileName)) {
         const projectRelativePath = path.relative(baseDir, fileName);
         filePath = path.resolve(declarationDir, projectRelativePath);
       }
