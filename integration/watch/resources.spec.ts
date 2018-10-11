bfc019faf5e6a910f633cd5eb1bdcf9c3264a623
Alan Agius <alan.agius4@gmail.com>
2018-10-11 20:50:52 +0200

    fix: update templates and styles in watch mode
    At the moment when changing a template or a style a recompilation won't happen as these external resources are not added as a dependency for the entry point

---
+++ integration/watch/resources.spec.ts
@@ -0,0 +1,48 @@
+import { TestHarness } from './test-harness';
+
+describe('resources', () => {
+  const harness = new TestHarness('resources');
+
+  before(async () => {
+    await harness.initialize();
+  });
+
+  afterEach(() => {
+    harness.reset();
+  });
+
+  after(() => {
+    harness.dispose();
+  });
+
+  describe('primary entrypoint', () => {
+    it("should perform initial compilation when 'watch' is started", () => {
+      harness.expectFesm5ToMatch('resources', /Angular!/);
+      harness.expectFesm2015ToMatch('resources', /Angular!/);
+      harness.expectFesm5ToMatch('resources', /a{color:#000}/);
+      harness.expectFesm2015ToMatch('resources', /a{color:#000}/);
+    });
+
+    describe('when file changes', () => {
+      it('should perform a partial compilation and emit the updated template', done => {
+        harness.copyTestCase('html-template');
+
+        harness.onComplete(() => {
+          harness.expectFesm5ToMatch('resources', /Hello World!/);
+          harness.expectFesm2015ToMatch('resources', /Hello World!/);
+          done();
+        });
+      });
+
+      it('should perform a partial compilation and emit the updated styles', done => {
+        harness.copyTestCase('scss-file');
+
+        harness.onComplete(() => {
+          harness.expectFesm5ToMatch('resources', /a{color:#fff}/);
+          harness.expectFesm2015ToMatch('resources', /a{color:#fff}/);
+          done();
+        });
+      });
+    });
+  });
+});
