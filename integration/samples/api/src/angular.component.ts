1bf8eaf589b41208000c459deb613bdb40e2cb6b
Alan Agius <alan.agius4@gmail.com>
2018-10-21 10:23:55 +0200

    feat: add support for `resolveJsonModule`
    Since version 7, Angular Compiler supports Json files and thus so should we.
    
    Couple of things to note: At the moment tsickle doesn't support this, thus for this feature to work you need to disable `annotateForClosureCompiler`.
    
    You'll also need to enable the following option in the `compilerOptions`
    ```
        "resolveJsonModule": true,
        "esModuleInterop": true,
        "allowSyntheticDefaultImports": true,
    ```
    
    Closes #1050 and Closes https://github.com/angular/angular-cli/issues/11931

--- integration/samples/api/src/angular.component.ts
+++ integration/samples/api/src/angular.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input } from '@angular/core';
+import jsonData from './angular.component.json';
 
 @Component({
   selector: 'ng-component',
-  template: '<h1>{{title}}</h1>',
+  template: '<h1>{{title}}</h1>{{data}}',
   styleUrls: ['./angular.component.scss']
 })
 export class AngularComponent {
-  @Input() title = 'Angular';
+  @Input()
+  title = 'Angular';
+
+  data = jsonData;
 }
