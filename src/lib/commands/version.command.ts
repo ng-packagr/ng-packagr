6ac0dcf40d2e9ed22c2e5f2b7c007062a3e96f2f
Alan Agius <alan.agius4@gmail.com>
2019-03-27 15:55:46 +0100

    fix: version command should not require tsickle

--- src/lib/commands/version.command.ts
+++ src/lib/commands/version.command.ts
@@ -3,14 +3,17 @@ import { VERSION as COMPILER_VERSION } from '@angular/compiler';
 import { version as TS_VERSION } from 'typescript';
 import { Command } from './command';
 
-const TSICKLE_VERSION = require('tsickle/package.json').version;
-
 /**
  * Prints version information.
  *
  * @stable
  */
 export const version: Command<any, void> = (pkg: any) => {
+  let TSICKLE_VERSION = '';
+  try {
+    TSICKLE_VERSION = require('tsickle/package.json').version;
+  } catch {}
+
   const NG_PACKAGR_VERSION = pkg ? pkg.version : 'unknown';
 
   console.log(`ng-packagr:            ` + NG_PACKAGR_VERSION);
