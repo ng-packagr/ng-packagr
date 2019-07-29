2156f5f1e7d0ef3c37013421d0e150d4b0430021
Alan Agius <alan.agius4@gmail.com>
2019-07-29 19:13:18 +0200

    fix: handle svg templates same as html files
    Closes: https://github.com/angular/angular-cli/issues/15187

--- integration/samples/custom/src/baz/baz.component.ts
+++ integration/samples/custom/src/baz/baz.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'custom-baz',
-  templateUrl: './baz.component.html',
+  templateUrl: './baz.component.svg',
   styleUrls: ['./baz.component.less', './baz.component.css'],
 })
 export class BazComponent {}
