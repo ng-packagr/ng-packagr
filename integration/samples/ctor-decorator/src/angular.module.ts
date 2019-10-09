de5894bbc6bd8d51d67ad202168e20819e3d86bb
Alan Agius <alan.agius4@gmail.com>
2019-10-09 08:02:48 +0200

    fix: tsickle references to non imported values when using Angular 8
    Closes: https://github.com/angular/angular-cli/issues/15789

--- integration/samples/ctor-decorator/src/angular.module.ts
+++ integration/samples/ctor-decorator/src/angular.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { AngularComponent } from './angular.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   declarations: [AngularComponent],
   exports: [AngularComponent],
 })
