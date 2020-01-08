8dd1b390e5b78b388d0d332eb8d3e3e47134a6de
Arvind Nama <arvind.nu@gmail.com>
2020-01-07 17:10:58 -0800

    feat: resolve custom module alias from ts-path

--- integration/samples/ts-paths/specs/paths.ts
+++ integration/samples/ts-paths/specs/paths.ts
@@ -7,19 +7,12 @@ describe(`@sample/ts-paths`, () => {
 
   describe('Resolve Ts-Path', () => {
     it(`should reslove import moduleSpecifier using ts-config paths`, () => {
-      expect(readFileSync(join(dist, 'bundles/sample-ts-paths.umd.js'), 'utf-8')).to.contain(
-        `function () { return 'sampleApi'; });`,
-      );
       expect(readFileSync(join(dist, 'esm5/src/angular.component.js'), 'utf-8')).to.contain(
         'import { getSampleApi } from "./api/index";',
       );
       expect(readFileSync(join(dist, 'esm2015/src/angular.component.js'), 'utf-8')).to.contain(
         'import { getSampleApi } from "./api/index";',
       );
-      expect(readFileSync(join(dist, 'fesm5/sample-ts-paths.js'), 'utf-8')).to.contain(
-        `function () { return 'sampleApi'; });`,
-      );
-      expect(readFileSync(join(dist, 'fesm2015/sample-ts-paths.js'), 'utf-8')).to.contain(`() => 'sampleApi'`);
     });
   });
 });
