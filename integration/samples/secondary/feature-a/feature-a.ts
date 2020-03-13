d6d1c088bb56fbc9093c17c0e59574db2f0ed069
Alan Agius <alan.agius4@gmail.com>
2020-03-13 12:07:38 +0100

    test: update angular version in consumers app

--- integration/samples/secondary/feature-a/feature-a.ts
+++ integration/samples/secondary/feature-a/feature-a.ts
@@ -2,3 +2,4 @@ import { SHARED_FEATURE } from '@sample/secondary/shared';
 
 export const FEATURE_A = `Yeah, ${SHARED_FEATURE}`;
 export const NAME = '@sample/secondary/feature-a';
+export { SHARED_FEATURE };
