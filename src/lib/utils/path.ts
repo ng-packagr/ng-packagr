5a48d77bb13fa58f427a502199175756e73ea317
Alan Agius <alan.agius4@gmail.com>
2020-08-14 09:52:11 +0200

    perf: cache normalized paths

--- src/lib/utils/path.ts
+++ src/lib/utils/path.ts
@@ -1,12 +1,21 @@
 import * as nodePath from 'path';
 
+const PATH_REGEXP = new RegExp('\\' + nodePath.win32.sep, 'g');
+const ensureUnixPathCache = new Map<string, string>();
+
 export const ensureUnixPath = (path?: string): string | null => {
   if (!path) {
     return null;
   }
 
+  const cachePath = ensureUnixPathCache.get(path);
+  if (cachePath) {
+    return cachePath;
+  }
+
   // we use a regex instead of the character literal due to a bug in some versions of node.js
   // the path separator needs to be preceded by an escape character
-  const regex = new RegExp('\\' + nodePath.win32.sep, 'g');
-  return path.replace(regex, nodePath.posix.sep);
+  const normalizedPath = path.replace(PATH_REGEXP, nodePath.posix.sep);
+  ensureUnixPathCache.set(path, normalizedPath);
+  return normalizedPath;
 };
