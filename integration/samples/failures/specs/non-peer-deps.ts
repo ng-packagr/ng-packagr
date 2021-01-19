b18d2e9120f073d4dfd248e551eda28cd6b062a3
Walter Werner SCHNEIDER <contact@schnwalter.eu>
2021-01-19 22:24:20 +0200

    test: fix missing dependency build cleanup test

--- integration/samples/failures/specs/non-peer-deps.ts
+++ integration/samples/failures/specs/non-peer-deps.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 describe(`Failure Builds`, () => {
   describe(`non-peer-deps`, () => {
     it(`should have no build output`, () => {
-      const exists = fs.existsSync(path.resolve(__dirname, 'dist'));
+      const exists = fs.existsSync(path.resolve(__dirname, '../dist'));
       expect(exists).to.be.false;
     });
   });
