dedb3b3de41c16e85fde6e39626a858bb6d473c1
nontangent <irregular0number@gmail.com>
2021-01-20 17:42:45 +0900

    feat: add stylesheet processor DI

--- src/lib/ng-package/entry-point/compile-ngc.di.ts
+++ src/lib/ng-package/entry-point/compile-ngc.di.ts
@@ -1,11 +1,15 @@
-import { InjectionToken } from 'injection-js';
+import { InjectionToken, Provider } from 'injection-js';
 import { Transform } from '../../graph/transform';
 import { TransformProvider, provideTransform } from '../../graph/transform.di';
-import { compileNgcTransform } from './compile-ngc.transform';
+import { compileNgcTransformFactory } from './compile-ngc.transform';
+import { STYLESHEET_PROCESSOR_TOKEN, STYLESHEET_PROCESSOR } from '../../styles/stylesheet-processor.di';
 
 export const COMPILE_NGC_TOKEN = new InjectionToken<Transform>(`ng.v5.compileNgcTransform`);
 
 export const COMPILE_NGC_TRANSFORM: TransformProvider = provideTransform({
   provide: COMPILE_NGC_TOKEN,
-  useFactory: () => compileNgcTransform,
+  useFactory: compileNgcTransformFactory,
+  deps: [STYLESHEET_PROCESSOR_TOKEN],
 });
+
+export const COMPILE_NGC_PROVIDERS: Provider[] = [STYLESHEET_PROCESSOR, COMPILE_NGC_TRANSFORM];
--- src/lib/ng-package/entry-point/entry-point.di.ts
+++ src/lib/ng-package/entry-point/entry-point.di.ts
@@ -1,7 +1,7 @@
 import { InjectionToken, Provider } from 'injection-js';
 import { Transform } from '../../graph/transform';
 import { TransformProvider, provideTransform } from '../../graph/transform.di';
-import { COMPILE_NGC_TOKEN, COMPILE_NGC_TRANSFORM } from './compile-ngc.di';
+import { COMPILE_NGC_TOKEN, COMPILE_NGC_PROVIDERS } from './compile-ngc.di';
 import { WRITE_BUNDLES_TRANSFORM, WRITE_BUNDLES_TRANSFORM_TOKEN } from './write-bundles.di';
 import { WRITE_PACKAGE_TRANSFORM, WRITE_PACKAGE_TRANSFORM_TOKEN } from './write-package.di';
 import { entryPointTransformFactory } from './entry-point.transform';
@@ -16,7 +16,7 @@ export const ENTRY_POINT_TRANSFORM: TransformProvider = provideTransform({
 
 export const ENTRY_POINT_PROVIDERS: Provider[] = [
   ENTRY_POINT_TRANSFORM,
-  COMPILE_NGC_TRANSFORM,
+  ...COMPILE_NGC_PROVIDERS,
   WRITE_BUNDLES_TRANSFORM,
   WRITE_PACKAGE_TRANSFORM,
 ];
