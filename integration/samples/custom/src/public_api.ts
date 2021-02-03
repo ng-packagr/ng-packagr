01ee80c9b64625270a7463fdcbff5a8a08236a70
Walter Werner SCHNEIDER <contact@schnwalter.eu>
2021-02-03 20:07:53 +0200

    chore: cleanup and fix consumer integration projects

--- integration/samples/custom/src/public_api.ts
+++ integration/samples/custom/src/public_api.ts
@@ -1,6 +1,9 @@
-export * from './foo/foo.component';
+/**
+ * @file Exports the public API defined by this module.
+ */
 export * from './bar/bar.component';
 export * from './baz/baz.component';
-export * from './foo-bar/foo-bar.component';
-export * from './less-baz/less-baz.component';
 export * from './custom.module';
+export * from './foo-bar/foo-bar.component';
+export * from './foo/foo.component';
+export * from './less-baz/less-baz.component';
--- integration/samples/material/public_api.ts
+++ integration/samples/material/public_api.ts
@@ -1,4 +1,8 @@
+/**
+ * @file Exports the public API defined by this module.
+ */
 export * from './bar';
-export * from './foo/foo.component';
 export * from './baz/baz.component';
+export * from './foo-bar/foo-bar.component';
+export * from './foo/foo.component';
 export * from './ui-lib.module';
