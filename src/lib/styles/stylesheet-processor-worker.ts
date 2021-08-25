480bbb40aec4b62f2fd80c3e8929f1ffcf7489af
Alan Agius <alan.agius4@gmail.com>
2021-08-25 11:26:32 +0200

    fix: make cache paths safe for windows

--- src/lib/styles/stylesheet-processor-worker.ts
+++ src/lib/styles/stylesheet-processor-worker.ts
@@ -168,7 +168,7 @@ function optimizeCss(filePath: string, css: string, browsers: string[], cssUrl?:
 }
 
 function generateKey(content: string, browserslistData: string[]): string {
-  return createHash('sha1').update(ngPackagrVersion).update(content).update(browserslistData.join('')).digest('base64');
+  return createHash('sha1').update(ngPackagrVersion).update(content).update(browserslistData.join('')).digest('hex');
 }
 
 async function readCacheEntry(cachePath: string, key: string): Promise<WorkerResult | undefined> {
