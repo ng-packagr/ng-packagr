5103e76ad39fb301f5fc459904a6ff99c8bb6a30
Alan Agius <alanagius@google.com>
2022-09-01 08:09:36 +0000

    fix: exclude scanning node_modules when trying to locate README.md
    Closes #2418

--- src/lib/ng-package/package.transform.ts
+++ src/lib/ng-package/package.transform.ts
@@ -1,5 +1,4 @@
 import { DepGraph } from 'dependency-graph';
-import * as path from 'path';
 import { NEVER, Observable, from, of as observableOf, pipe } from 'rxjs';
 import {
   catchError,
@@ -20,7 +19,7 @@ import { BuildGraph } from '../graph/build-graph';
 import { STATE_IN_PROGRESS } from '../graph/node';
 import { Transform } from '../graph/transform';
 import { colors } from '../utils/color';
-import { copyFile, rmdir, stat } from '../utils/fs';
+import { rmdir } from '../utils/fs';
 import * as log from '../utils/log';
 import { ensureUnixPath } from '../utils/path';
 import { discoverPackages } from './discover-packages';
@@ -46,7 +45,6 @@ import { NgPackagrOptions } from './options.di';
  *  - analyzeTsSources (thereby extracting template and stylesheet files)
  *  - for each entry point
  *    - run the entryPontTransform
- *  - writeNpmPackage
  *
  * @param project Project token, reference to `ng-package.json`
  * @param options ng-packagr options
@@ -54,211 +52,187 @@ import { NgPackagrOptions } from './options.di';
  * @param analyseSourcesTransform Transformation analyzing the typescript source files of each entry point.
  * @param entryPointTransform Transformation for asset rendering and compilation of a single entry point.
  */
-export const packageTransformFactory = (
-  project: string,
-  options: NgPackagrOptions,
-  initTsConfigTransform: Transform,
-  analyseSourcesTransform: Transform,
-  entryPointTransform: Transform,
-) => (source$: Observable<BuildGraph>): Observable<BuildGraph> => {
-  const pkgUri = ngUrl(project);
+export const packageTransformFactory =
+  (
+    project: string,
+    options: NgPackagrOptions,
+    initTsConfigTransform: Transform,
+    analyseSourcesTransform: Transform,
+    entryPointTransform: Transform,
+  ) =>
+  (source$: Observable<BuildGraph>): Observable<BuildGraph> => {
+    const pkgUri = ngUrl(project);
 
-  const buildTransform = options.watch
-    ? watchTransformFactory(project, options, analyseSourcesTransform, entryPointTransform)
-    : buildTransformFactory(project, analyseSourcesTransform, entryPointTransform);
+    const buildTransform = options.watch
+      ? watchTransformFactory(project, options, analyseSourcesTransform, entryPointTransform)
+      : buildTransformFactory(project, analyseSourcesTransform, entryPointTransform);
 
-  return source$.pipe(
-    tap(() => log.info(`Building Angular Package`)),
-    // Discover packages and entry points
-    switchMap(graph => {
-      const pkg = discoverPackages({ project });
+    return source$.pipe(
+      tap(() => log.info(`Building Angular Package`)),
+      // Discover packages and entry points
+      switchMap(graph => {
+        const pkg = discoverPackages({ project });
 
-      return from(pkg).pipe(
-        map(value => {
-          const ngPkg = new PackageNode(pkgUri);
-          ngPkg.data = value;
+        return from(pkg).pipe(
+          map(value => {
+            const ngPkg = new PackageNode(pkgUri);
+            ngPkg.data = value;
 
-          return graph.put(ngPkg);
-        }),
-      );
-    }),
-    // Clean the primary dest folder (should clean all secondary sub-directory, as well)
-    switchMap(
-      async graph => {
-        const { dest, deleteDestPath } = graph.get(pkgUri).data;
-
-        if (deleteDestPath) {
-          try {
-            await rmdir(dest, { recursive: true });
-          } catch {}
-        }
-      },
-      (graph, _) => graph,
-    ),
-    // Add entry points to graph
-    map(graph => {
-      const foundNode = graph.get(pkgUri);
-      if (!isPackage(foundNode)) {
-        return graph;
-      }
-
-      const ngPkg: PackageNode = foundNode;
-      const entryPoints = [ngPkg.data.primary, ...ngPkg.data.secondaries].map(entryPoint => {
-        const { destinationFiles, moduleId } = entryPoint;
-        const node = new EntryPointNode(
-          ngUrl(moduleId),
-          ngPkg.cache.sourcesFileCache,
-          ngPkg.cache.ngccProcessingCache,
-          ngPkg.cache.moduleResolutionCache,
+            return graph.put(ngPkg);
+          }),
         );
-        node.data = { entryPoint, destinationFiles };
-        node.state = 'dirty';
-        ngPkg.dependsOn(node);
+      }),
+      // Clean the primary dest folder (should clean all secondary sub-directory, as well)
+      switchMap(
+        async graph => {
+          const { dest, deleteDestPath } = graph.get(pkgUri).data;
 
-        return node;
-      });
+          if (deleteDestPath) {
+            try {
+              await rmdir(dest, { recursive: true });
+            } catch {}
+          }
+        },
+        (graph, _) => graph,
+      ),
+      // Add entry points to graph
+      map(graph => {
+        const foundNode = graph.get(pkgUri);
+        if (!isPackage(foundNode)) {
+          return graph;
+        }
 
-      return graph.put(entryPoints);
-    }),
-    // Initialize the tsconfig for each entry point
-    initTsConfigTransform,
-    // perform build
-    buildTransform,
-  );
-};
+        const ngPkg: PackageNode = foundNode;
+        const entryPoints = [ngPkg.data.primary, ...ngPkg.data.secondaries].map(entryPoint => {
+          const { destinationFiles, moduleId } = entryPoint;
+          const node = new EntryPointNode(
+            ngUrl(moduleId),
+            ngPkg.cache.sourcesFileCache,
+            ngPkg.cache.ngccProcessingCache,
+            ngPkg.cache.moduleResolutionCache,
+          );
+          node.data = { entryPoint, destinationFiles };
+          node.state = 'dirty';
+          ngPkg.dependsOn(node);
 
-const watchTransformFactory = (
-  project: string,
-  _options: NgPackagrOptions,
-  analyseSourcesTransform: Transform,
-  entryPointTransform: Transform,
-) => (source$: Observable<BuildGraph>): Observable<BuildGraph> => {
-  const CompleteWaitingForFileChange = '\nCompilation complete. Watching for file changes...';
-  const FileChangeDetected = '\nFile change detected. Starting incremental compilation...';
-  const FailedWaitingForFileChange = '\nCompilation failed. Watching for file changes...';
+          return node;
+        });
 
-  return source$.pipe(
-    switchMap(graph => {
-      const { data, cache } = graph.find(isPackage);
-      const { onFileChange, watcher } = createFileWatch([], [data.dest]);
-      graph.watcher = watcher;
+        return graph.put(entryPoints);
+      }),
+      // Initialize the tsconfig for each entry point
+      initTsConfigTransform,
+      // perform build
+      buildTransform,
+    );
+  };
 
-      return onFileChange.pipe(
-        tap(fileChange => {
-          const { filePath, event } = fileChange;
-          const { sourcesFileCache } = cache;
-          const cachedSourceFile = sourcesFileCache.get(filePath);
-          const { declarationFileName } = cachedSourceFile || {};
-          const uriToClean = [filePath, declarationFileName].map(x => fileUrl(ensureUnixPath(x)));
-          const nodesToClean = graph.filter(node => uriToClean.some(uri => uri === node.url));
+const watchTransformFactory =
+  (project: string, _options: NgPackagrOptions, analyseSourcesTransform: Transform, entryPointTransform: Transform) =>
+  (source$: Observable<BuildGraph>): Observable<BuildGraph> => {
+    const CompleteWaitingForFileChange = '\nCompilation complete. Watching for file changes...';
+    const FileChangeDetected = '\nFile change detected. Starting incremental compilation...';
+    const FailedWaitingForFileChange = '\nCompilation failed. Watching for file changes...';
 
-          if (!cachedSourceFile) {
+    return source$.pipe(
+      switchMap(graph => {
+        const { data, cache } = graph.find(isPackage);
+        const { onFileChange, watcher } = createFileWatch([], [data.dest]);
+        graph.watcher = watcher;
+
+        return onFileChange.pipe(
+          tap(fileChange => {
+            const { filePath, event } = fileChange;
+            const { sourcesFileCache } = cache;
+            const cachedSourceFile = sourcesFileCache.get(filePath);
+            const { declarationFileName } = cachedSourceFile || {};
+            const uriToClean = [filePath, declarationFileName].map(x => fileUrl(ensureUnixPath(x)));
+            const nodesToClean = graph.filter(node => uriToClean.some(uri => uri === node.url));
+
+            if (!cachedSourceFile) {
+              if (event === 'unlink' || event === 'add') {
+                cache.globCache = regenerateGlobCache(sourcesFileCache);
+              }
+
+              if (!nodesToClean) {
+                return;
+              }
+            }
+
+            const allNodesToClean = [
+              ...nodesToClean,
+              // if a non ts file changes we need to clean up its direct dependees
+              // this is mainly done for resources such as html and css
+              ...nodesToClean.filter(node => !node.url.endsWith('.ts')).flatMap(node => [...node.dependees]),
+            ];
+
+            // delete node that changes
+            for (const { url } of allNodesToClean) {
+              sourcesFileCache.delete(fileUrlPath(url));
+            }
+
+            for (const entryPoint of graph.filter(isEntryPoint)) {
+              const isDirty = [...allNodesToClean].some(dependent => entryPoint.dependents.has(dependent));
+              if (isDirty) {
+                entryPoint.state = 'dirty';
+
+                uriToClean.forEach(url => {
+                  entryPoint.cache.analysesSourcesFileCache.delete(fileUrlPath(url));
+                });
+              }
+            }
+
+            // Regenerate glob cache
             if (event === 'unlink' || event === 'add') {
               cache.globCache = regenerateGlobCache(sourcesFileCache);
             }
+          }),
+          debounceTime(200),
+          tap(() => log.msg(FileChangeDetected)),
+          startWith(undefined),
+          mapTo(graph),
+        );
+      }),
+      switchMap(graph => {
+        return observableOf(graph).pipe(
+          buildTransformFactory(project, analyseSourcesTransform, entryPointTransform),
+          tap(() => log.msg(CompleteWaitingForFileChange)),
+          catchError(error => {
+            log.error(error);
+            log.msg(FailedWaitingForFileChange);
 
-            if (!nodesToClean) {
-              return;
-            }
-          }
+            return NEVER;
+          }),
+        );
+      }),
+    );
+  };
 
-          const allNodesToClean = [
-            ...nodesToClean,
-            // if a non ts file changes we need to clean up its direct dependees
-            // this is mainly done for resources such as html and css
-            ...nodesToClean.filter(node => !node.url.endsWith('.ts')).flatMap(node => [...node.dependees]),
-          ];
+const buildTransformFactory =
+  (project: string, analyseSourcesTransform: Transform, entryPointTransform: Transform) =>
+  (source$: Observable<BuildGraph>): Observable<BuildGraph> => {
+    const startTime = Date.now();
 
-          // delete node that changes
-          for (const { url } of allNodesToClean) {
-            sourcesFileCache.delete(fileUrlPath(url));
-          }
+    const pkgUri = ngUrl(project);
 
-          for (const entryPoint of graph.filter(isEntryPoint)) {
-            const isDirty = [...allNodesToClean].some(dependent => entryPoint.dependents.has(dependent));
-            if (isDirty) {
-              entryPoint.state = 'dirty';
-
-              uriToClean.forEach(url => {
-                entryPoint.cache.analysesSourcesFileCache.delete(fileUrlPath(url));
-              });
-            }
-          }
-
-          // Regenerate glob cache
-          if (event === 'unlink' || event === 'add') {
-            cache.globCache = regenerateGlobCache(sourcesFileCache);
-          }
-        }),
-        debounceTime(200),
-        tap(() => log.msg(FileChangeDetected)),
-        startWith(undefined),
-        mapTo(graph),
-      );
-    }),
-    switchMap(graph => {
-      return observableOf(graph).pipe(
-        buildTransformFactory(project, analyseSourcesTransform, entryPointTransform),
-        tap(() => log.msg(CompleteWaitingForFileChange)),
-        catchError(error => {
-          log.error(error);
-          log.msg(FailedWaitingForFileChange);
-
-          return NEVER;
-        }),
-      );
-    }),
-  );
-};
-
-const buildTransformFactory = (project: string, analyseSourcesTransform: Transform, entryPointTransform: Transform) => (
-  source$: Observable<BuildGraph>,
-): Observable<BuildGraph> => {
-  const startTime = Date.now();
-
-  const pkgUri = ngUrl(project);
-
-  return source$.pipe(
-    // Analyse dependencies and external resources for each entry point
-    analyseSourcesTransform,
-    // Next, run through the entry point transformation (assets rendering, code compilation)
-    scheduleEntryPoints(entryPointTransform),
-    // Write npm package to dest folder
-    writeNpmPackage(pkgUri),
-    tap(graph => {
-      const ngPkg = graph.get(pkgUri);
-      log.success('\n------------------------------------------------------------------------------');
-      log.success(`Built Angular Package
+    return source$.pipe(
+      // Analyse dependencies and external resources for each entry point
+      analyseSourcesTransform,
+      // Next, run through the entry point transformation (assets rendering, code compilation)
+      scheduleEntryPoints(entryPointTransform),
+      tap(graph => {
+        const ngPkg = graph.get(pkgUri);
+        log.success('\n------------------------------------------------------------------------------');
+        log.success(`Built Angular Package
  - from: ${ngPkg.data.src}
  - to:   ${ngPkg.data.dest}`);
-      log.success('------------------------------------------------------------------------------');
-      const b = colors.bold;
-      const w = colors.white;
-      log.msg(w(`\nBuild at: ${b(new Date().toISOString())} - Time: ${b('' + (Date.now() - startTime))}ms\n`));
-    }),
-  );
-};
-
-const writeNpmPackage = (pkgUri: string): Transform =>
-  pipe(
-    switchMap(async graph => {
-      const { data } = graph.get(pkgUri);
-      const srcFiles = [`${data.src}/LICENSE`, `${data.src}/README.md`];
-
-      for (const srcFile of srcFiles) {
-        let isFile = false;
-        try {
-          isFile = (await stat(srcFile)).isFile();
-        } catch {}
-
-        if (isFile) {
-          await copyFile(srcFile, path.join(data.dest, path.basename(srcFile)));
-        }
-      }
-
-      return graph;
-    }),
-  );
+        log.success('------------------------------------------------------------------------------');
+        const b = colors.bold;
+        const w = colors.white;
+        log.msg(w(`\nBuild at: ${b(new Date().toISOString())} - Time: ${b('' + (Date.now() - startTime))}ms\n`));
+      }),
+    );
+  };
 
 const scheduleEntryPoints = (epTransform: Transform): Transform =>
   pipe(
