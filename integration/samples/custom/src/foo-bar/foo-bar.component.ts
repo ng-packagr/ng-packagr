2f92ab8e6dabc75d6e4f5793b7de6115848bdf6c
Alan Agius <alanagius@google.com>
2022-09-16 11:51:42 +0000

    refactor: remove support for Stylus
    BREAKING CHANGE:
    
    Deprecated support for Stylus has been removed. The Stylus package has never reached a stable version and it's usage in the ng-packagr is minimal. It's recommended to migrate to another CSS preprocessor that the ng-packagr supports.

--- integration/samples/custom/src/foo-bar/foo-bar.component.ts
+++ integration/samples/custom/src/foo-bar/foo-bar.component.ts
@@ -3,7 +3,6 @@ import { Component, ContentChild, ElementRef } from '@angular/core';
 @Component({
   selector: 'custom-foo-bar',
   templateUrl: './foo-bar.component.html',
-  styleUrls: ['./foo-bar.component.styl'],
 })
 export class FooBarComponent {
   @ContentChild('heading', { read: ElementRef, static: true })
--- integration/samples/scss-paths/sub-module/bar/bar.component.ts
+++ integration/samples/scss-paths/sub-module/bar/bar.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bar-component',
   templateUrl: './bar.component.html',
-  styleUrls: ['./bar.component.scss', './bar.component.less', './bar.component.styl', './bar.component.sass'],
+  styleUrls: ['./bar.component.scss', './bar.component.less', './bar.component.sass'],
 })
 export class BarComponent {}
