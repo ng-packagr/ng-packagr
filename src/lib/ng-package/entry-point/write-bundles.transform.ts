f9fafd4f387e7ec5e0b68b64d34abec9629e5489
Alan Agius <alanagius@google.com>
2024-05-22 12:14:08 +0000

    Revert "perf: use esbuild instead of rollup to generate FESM"
    This reverts commit 9c238273c9057108744c9f9ef8c7d9e62225c0f1.

--- src/lib/ng-package/entry-point/write-bundles.transform.ts
+++ src/lib/ng-package/entry-point/write-bundles.transform.ts
@@ -1,13 +1,22 @@
 import ora from 'ora';
-import { bundle } from '../../flatten/esbuild';
+import { join } from 'path';
+import type { OutputAsset, OutputChunk, RollupCache } from 'rollup';
+import { rollupBundleFile } from '../../flatten/rollup';
 import { transformFromPromise } from '../../graph/transform';
+import { generateKey, readCacheEntry, saveCacheEntry } from '../../utils/cache';
+import { exists, mkdir, writeFile } from '../../utils/fs';
 import { EntryPointNode, isEntryPointInProgress } from '../nodes';
 import { NgPackagrOptions } from '../options.di';
+
+interface BundlesCache {
+  hash: string;
+  fesm2022: (OutputChunk | OutputAsset)[];
+}
 
 export const writeBundlesTransform = (options: NgPackagrOptions) =>
   transformFromPromise(async graph => {
     const entryPoint: EntryPointNode = graph.find(isEntryPointInProgress());
-    const { destinationFiles, entryPoint: ngEntryPoint } = entryPoint.data;
+    const { destinationFiles, entryPoint: ngEntryPoint, tsConfig } = entryPoint.data;
     const cache = entryPoint.cache;
     const { fesm2022Dir, esm2022 } = destinationFiles;
 
@@ -16,22 +25,92 @@ export const writeBundlesTransform = (options: NgPackagrOptions) =>
       discardStdin: false,
     });
 
-    try {
-      const result = await bundle({
-        buildContext: cache.esbuildBuildContext,
+    const key = await generateKey(ngEntryPoint.moduleId, esm2022, fesm2022Dir, tsConfig.options.compilationMode);
+    const hash = await generateKey([...cache.outputCache.values()].map(({ version }) => version).join(':'));
+    const cacheDirectory = options.cacheEnabled && options.cacheDirectory;
+    if (cacheDirectory) {
+      const cacheResult: BundlesCache = await readCacheEntry(options.cacheDirectory, key);
+      let writing = false;
+
+      if (cacheResult?.hash === hash) {
+        try {
+          for (const file of cacheResult.fesm2022) {
+            const filePath = join(fesm2022Dir, file.fileName);
+            if (options.watch && await exists(filePath)) {
+              continue;
+            }
+
+            if (!writing) {
+              writing = true;
+              spinner.start('Writing FESM bundles');
+              await mkdir(fesm2022Dir, { recursive: true });
+            }
+
+            await writeFile(filePath, file.type === 'asset' ? file.source : file.code);
+          }
+
+          if (writing) {
+            spinner.succeed('Writing FESM bundles');
+          }
+        } catch (error) {
+          if (!writing) {
+            spinner.start('Writing FESM bundles');
+          }
+
+          spinner.fail();
+          throw error;
+        }
+
+        return;
+      }
+    }
+
+    async function generateFESM(
+      rollupCache: RollupCache,
+      dir: string,
+    ): Promise<{ files: (OutputChunk | OutputAsset)[]; rollupCache: RollupCache }> {
+      const { cache: rollupFESMCache, files } = await rollupBundleFile({
+        sourceRoot: tsConfig.options.sourceRoot,
         entry: esm2022,
-        outdir: fesm2022Dir,
         entryName: ngEntryPoint.flatModuleFile,
         moduleName: ngEntryPoint.moduleId,
-        watch: options.watch,
+        dir,
+        cache: rollupCache,
+        cacheDirectory,
         fileCache: cache.outputCache,
+        cacheKey: await generateKey(esm2022, dir, ngEntryPoint.moduleId, tsConfig.options.compilationMode),
       });
 
-      cache.esbuildBuildContext = result.esbuildContext;
+      return {
+        /** The map contents are in an asset file type, which makes storing the map in the cache as redudant. */
+        files: files.map(f => {
+          if (f.type === 'chunk') {
+            f.map = null;
+          }
+
+          return f;
+        }),
+        rollupCache: options.watch ? rollupFESMCache : undefined,
+      };
+    }
+
+    const fesmCache: Partial<BundlesCache> = {
+      hash,
+    };
+
+    try {
+      const { rollupCache, files } = await generateFESM(cache.rollupFESM2022Cache, fesm2022Dir);
+
+      cache.rollupFESM2022Cache = rollupCache;
+      fesmCache.fesm2022 = files;
 
       spinner.succeed(`Generating FESM bundles`);
     } catch (error) {
       spinner.fail();
       throw error;
+    }
+
+    if (cacheDirectory) {
+      await saveCacheEntry(cacheDirectory, key, JSON.stringify(fesmCache));
     }
   });
