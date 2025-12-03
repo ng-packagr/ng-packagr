2352e584e82fffe92f18cef55a74fc22e2542bd2
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2025-12-03 09:14:04 +0000

    build: rename commitlint rule `subject-max-length` to `header-max-length`

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
-    'subject-max-length': [2, 'always', 120],
+    'header-max-length': [2, 'always', 120],
     'scope-enum': [2, 'always', []],
     'scope-empty': [2, 'always'],
     'type-enum': [
