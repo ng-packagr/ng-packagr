e55bcc6c99dd1ae36c14d50a15441ed07f43d8a7
Alan Agius <17563226+alan-agius4@users.noreply.github.com>
2026-03-30 09:28:43 +0000

    ci: switch from commitlint to ng-dev for code quality
    - Add code format check to CI using ng-dev
    - Remove commitlint and .husky/commit-msg hook
    - Add @angular/ng-dev dependency and config files

---
+++ .ng-dev/config.mjs
@@ -0,0 +1,6 @@
+import { commitMessage } from './commit-message.mjs';
+import { format } from './format.mjs';
+import { github } from './github.mjs';
+import { pullRequest } from './pull-request.mjs';
+
+export { commitMessage, format, github, pullRequest };
