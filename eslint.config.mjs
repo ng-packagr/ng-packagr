597b94e234bf908111d0711502de4aea1906472a
David Herges <dherges@users.noreply.github.com>
2026-05-12 01:11:10 +0200

    build: optimize eslint.config.mjs
    Co-authored-by: Alan Agius <alan.agius4@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,14 +6,11 @@ import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
 });
