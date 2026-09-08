062bf8a0d74e06c27a201c37ee1e4ed948e5cdb3
Alan Agius <alanagius@google.com>
2026-09-08 07:22:39 +0000

    build: replace pnpm -s shorthand with --silent flag in CI workflows and integration scripts
    Prepare for pnpm 12.

--- integration/consumers.sh
+++ integration/consumers.sh
@@ -6,8 +6,8 @@ echo "Running consumer builds in $PWD"
 pushd consumers/ng-cli
 
 # Build ng cli app
-pnpm -s install
-pnpm -s build:dev --output-path dist/dev
-pnpm -s build:prod:jit --output-path dist/jit
-pnpm -s build:prod:aot --output-path dist/aot
+pnpm --silent install
+pnpm --silent build:dev --output-path dist/dev
+pnpm --silent build:prod:jit --output-path dist/jit
+pnpm --silent build:prod:aot --output-path dist/aot
 popd
